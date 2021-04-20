c8e5d5a81668dae7e029a9d0c94cf3ef95b6d381
ishii-norimi <mirasunimoni@yahoo.co.jp>
2021-04-20 23:39:12 +0900

    Fix KZ filter

--- model/kz.js
+++ model/kz.js
@@ -7,17 +7,19 @@ class KolmogorovZurbenkoFilter {
 
 	_ma(x) {
 		const p = []
+		const n = x.length
 		const d = x[0].length
 		for (let i = 0; i < x.length; i++) {
-			const t = Math.min(this._m, i + 1)
+			const t1 = i - Math.floor((this._m - 1) / 2)
+			const t2 = t1 + this._m
 			p[i] = Array(d).fill(0)
-			for (let k = i - t + 1; k <= i; k++) {
+			for (let k = Math.max(0, t1); k < Math.min(n, t2); k++) {
 				for (let j = 0; j < d; j++) {
 					p[i][j] += x[k][j]
 				}
 			}
 			for (let j = 0; j < d; j++) {
-				p[i][j] /= t
+				p[i][j] /= Math.min(n, t2) - Math.max(0, t1)
 			}
 		}
 		return p
