2c6e4969b8f1d87e32f38b032da1598bd85a9fd3
ishii-norimi <mirasunimoni@yahoo.co.jp>
2021-06-13 13:47:52 +0900

    Add adaptive thresholding with gaussian

--- model/adaptive_thresholding.js
+++ model/adaptive_thresholding.js
@@ -1,29 +1,57 @@
 class AdaptiveThresholding {
 	// https://algorithm.joho.info/image-processing/adaptive-thresholding/
+	// https://docs.opencv.org/master/d7/d4d/tutorial_py_thresholding.html
 	constructor(method = 'mean', k = 3, c = 2) {
 		this._method = method
 		this._k = k
 		this._c = c
 	}
 
+	_kernel() {
+		const k = []
+		for (let i = 0; i < this._k; k[i++] = Array(this._k).fill(1 / (this._k ** 2)));
+		if (this._method === 'gaussian') {
+			const g = []
+			let gsum = 0
+			for (let i = 0; i < this._k; i++) {
+				g[i] = Math.exp(-((i - (this._k - 1) / 2) ** 2) / 2)
+				gsum += g[i]
+			}
+			for (let i = 0; i < this._k; i++) {
+				for (let j = 0; j < this._k; j++) {
+					k[i][j] = g[i] * g[j] / (gsum ** 2)
+				}
+			}
+		}
+		return k
+	}
+
 	predict(x) {
-		const offset = Math.floor(this._k / 2)
 		const p = []
+		const kernel = this._kernel()
 		for (let i = 0; i < x.length; i++) {
 			p[i] = []
 			for (let j = 0; j < x[i].length; j++) {
 				const d = x[i][j].length
 				const m = Array(d).fill(0)
-				let n = 0
-				for (let s = Math.max(0, i - offset); s <= Math.min(x.length - 1, i + offset); s++) {
-					for (let t = Math.max(0, j - offset); t <= Math.min(x[i].length - 1, j + offset); t++) {
-						for (let u = 0; u < d; u++) {
-							m[u] += x[s][t][u]
+				let ksum = 0
+				for (let s = 0; s < kernel.length; s++) {
+					const s0 = i + s - (kernel.length - 1) / 2
+					if (s0 < 0 || x.length <= s0) {
+						continue
+					}
+					for (let t = 0; t < kernel[s].length; t++) {
+						const t0 = j + t - (kernel[s].length - 1) / 2
+						if (t0 < 0 || x[i].length <= t0) {
+							continue
 						}
-						n++
+						for (let u = 0; u < d; u++) {
+							m[u] += kernel[s][t] * x[s0][t0][u]
+						}
+						ksum += kernel[s][t]
 					}
 				}
-				p[i][j] = m.map((v, u) => x[i][j][u] < v / n - this._c ? 0 : 255)
+				p[i][j] = m.map((v, u) => x[i][j][u] < v / ksum - this._c ? 0 : 255)
 			}
 		}
 		return p
@@ -46,7 +74,7 @@ var dispAdaptiveThresholding = function(elm, platform) {
 	elm.append("select")
 		.attr("name", "method")
 		.selectAll("option")
-		.data(["mean"])
+		.data(["mean", "gaussian"])
 		.enter()
 		.append("option")
 		.property("value", d => d)
