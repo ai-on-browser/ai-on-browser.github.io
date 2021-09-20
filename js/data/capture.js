30654e63dd73a2099100e170191838ef10aeff96
ishii-norimi <mirasunimoni@yahoo.co.jp>
2021-09-20 10:17:00 +0900

    Show thumbnail of image data

--- js/data/capture.js
+++ js/data/capture.js
@@ -7,16 +7,19 @@ export default class CaptureData extends ImageData {
 		this._size = [240, 360]
 
 		const elm = this.setting.data.configElement
-		this._mngelm = elm.append("div")
-		this._mngelm.append("input")
-			.attr("type", "button")
-			.attr("value", "Add data")
-			.on("click", () => this.startVideo())
-		this._slctImg = this._mngelm.append("select")
-			.on("change", () => {
-				this._manager.platform.render()
-			})
-		this._videoElm = elm.append("div")
+		this._mngelm = elm.append('div')
+		this._mngelm
+			.append('input')
+			.attr('type', 'button')
+			.attr('value', 'Add data')
+			.on('click', () => this.startVideo())
+		this._slctImg = this._mngelm.append('select').on('change', () => {
+			this._manager.platform.render()
+			this._thumbnail.selectAll('*').remove()
+			this._thumbnail.node().append(this._createCanvas(this.x[0]))
+		})
+		this._thumbnail = this._mngelm.append('span')
+		this._videoElm = elm.append('div')
 		this.startVideo()
 
 		this._x = []
@@ -24,11 +27,11 @@ export default class CaptureData extends ImageData {
 	}
 
 	get availTask() {
-		return ["SG", "DN", "ED"]
+		return ['SG', 'DN', 'ED']
 	}
 
 	get x() {
-		const idx = +this._slctImg.property("value") - 1
+		const idx = +this._slctImg.property('value') - 1
 		if (this._x.length === 0 || !this._x[idx]) {
 			return []
 		}
@@ -36,35 +39,43 @@ export default class CaptureData extends ImageData {
 	}
 
 	startVideo() {
-		this._mngelm.style("display", "none")
-		this._videoElm.append("div").text("Click video to use as data.")
-		this._video = this._videoElm.append("video")
-			.attr("width", this._size[1])
-			.attr("height", this._size[0])
-			.property("autoplay", true)
-			.on("click", () => {
+		this._mngelm.style('display', 'none')
+		this._videoElm.append('div').text('Click video to use as data.')
+		this._video = this._videoElm
+			.append('video')
+			.attr('width', this._size[1])
+			.attr('height', this._size[0])
+			.property('autoplay', true)
+			.on('click', () => {
 				this.readImage(this._video, image => {
 					this._x.push(image)
 					this._y.push(0)
-					this._slctImg.append("option").attr("value", this._x.length).text(this._x.length)
-					this._slctImg.property("value", this._x.length)
+					this._slctImg.append('option').attr('value', this._x.length).text(this._x.length)
+					this._slctImg.property('value', this._x.length)
+					this._thumbnail.selectAll('*').remove()
+					this._thumbnail.node().append(this._createCanvas(image))
 
 					this.stopVideo()
-					this._mngelm.style("display", null)
+					this._mngelm.style('display', null)
 					if (this._manager.platform.render) {
 						setTimeout(() => {
 							this._manager.platform.render()
 						}, 0)
 					}
 				})
-			}).node()
+			})
+			.node()
 
-		navigator.mediaDevices.getDisplayMedia({video: true}).then(stream => {
-			this._video.srcObject = stream
-		}).catch((e) => {
-			console.error(e)
-			this.stopVideo()
-		})
+		navigator.mediaDevices
+			.getDisplayMedia({ video: true })
+			.then(stream => {
+				this._video.srcObject = stream
+			})
+			.catch(e => {
+				console.error(e)
+				this.stopVideo()
+				this._mngelm.style('display', null)
+			})
 	}
 
 	stopVideo() {
@@ -78,7 +89,7 @@ export default class CaptureData extends ImageData {
 			}
 			this._video = null
 		}
-		this._videoElm.selectAll("*").remove()
+		this._videoElm.selectAll('*').remove()
 	}
 
 	terminate() {
--- js/data/image.js
+++ js/data/image.js
@@ -7,70 +7,70 @@ export default class ImageData extends BaseData {
 
 	static get colorSpaces() {
 		return {
-			RGB: "rgb",
-			COL8: "8 colors",
-			GRAY: "gray",
-			BINARY: "binary",
-			HLS: "hls",
-			HSV: "hsv"
+			RGB: 'rgb',
+			COL8: '8 colors',
+			GRAY: 'gray',
+			BINARY: 'binary',
+			HLS: 'hls',
+			HSV: 'hsv',
 		}
 	}
 
 	static get grayScales() {
 		return {
 			AVERAGE: {
-				name: "average",
-				calc: (r, g, b) => (r + g + b) / 3
+				name: 'average',
+				calc: (r, g, b) => (r + g + b) / 3,
 			},
 			AMMA_AVERAGE: {
-				name: "Gamma correction average",
-				calc: (r, g, b) => (r + g + b) / 3
+				name: 'Gamma correction average',
+				calc: (r, g, b) => (r + g + b) / 3,
 			},
 			CIEXYZ: {
-				name: "CIE XYZ",
-				calc: (r, g, b) => 0.2126 * r + 0.7152 * g + 0.0722 * b
+				name: 'CIE XYZ',
+				calc: (r, g, b) => 0.2126 * r + 0.7152 * g + 0.0722 * b,
 			},
 			BT601: {
-				name: "ITU-R Rec BT.601",
-				calc: (r, g, b) => 0.299 * r + 0.587 * g + 0.114 * b
+				name: 'ITU-R Rec BT.601',
+				calc: (r, g, b) => 0.299 * r + 0.587 * g + 0.114 * b,
 			},
 			BT601_ROUGH1: {
-				name: "Second decimal place of BT.601",
-				calc: (r, g, b) => 0.3 * r + 0.59 * g + 0.11 * b
+				name: 'Second decimal place of BT.601',
+				calc: (r, g, b) => 0.3 * r + 0.59 * g + 0.11 * b,
 			},
 			BT601_ROUGH2: {
-				name: "First decimal place of BT.601",
-				calc: (r, g, b) => 0.3 * r + 0.6 * g + 0.1 * b
+				name: 'First decimal place of BT.601',
+				calc: (r, g, b) => 0.3 * r + 0.6 * g + 0.1 * b,
 			},
 			BT709: {
-				name: "ITU-R Rec BT.709",
-				calc: (r, g, b) => 0.2126 * r + 0.7152 * g + 0.0722 * b
+				name: 'ITU-R Rec BT.709',
+				calc: (r, g, b) => 0.2126 * r + 0.7152 * g + 0.0722 * b,
 			},
 			YCgCo: {
-				name: "Y of YCgCo",
-				calc: (r, g, b) => r / 4 + g / 2 + b / 4
+				name: 'Y of YCgCo',
+				calc: (r, g, b) => r / 4 + g / 2 + b / 4,
 			},
 			HSV: {
-				name: "V of HSV",
-				calc: (r, g, b) => Math.max(r, g, b)
+				name: 'V of HSV',
+				calc: (r, g, b) => Math.max(r, g, b),
 			},
 			HLS: {
-				name: "V of HSV",
-				calc: (r, g, b) => (Math.max(r, g, b) + Math.min(r, g, b)) / 2
+				name: 'V of HSV',
+				calc: (r, g, b) => (Math.max(r, g, b) + Math.min(r, g, b)) / 2,
 			},
 			G: {
-				name: "Green",
-				calc: (r, g, b) => g
+				name: 'Green',
+				calc: (r, g, b) => g,
 			},
 			B: {
-				name: "Blue",
-				calc: (r, g, b) => b
-			}
+				name: 'Blue',
+				calc: (r, g, b) => b,
+			},
 		}
 	}
 
 	get availTask() {
-		return ["SG", "DN", "ED"]
+		return ['SG', 'DN', 'ED']
 	}
 
 	readImage(data, cb) {
@@ -81,7 +81,7 @@ export default class ImageData extends BaseData {
 				const image = new Image()
 				image.src = reader.result
 				image.onload = () => {
-					const canvas = document.createElement("canvas")
+					const canvas = document.createElement('canvas')
 					canvas.width = image.width
 					canvas.height = image.height
 					const context = canvas.getContext('2d')
@@ -91,14 +91,19 @@ export default class ImageData extends BaseData {
 					for (let i = 0, c = 0; i < canvas.height; i++) {
 						data[i] = []
 						for (let j = 0; j < canvas.width; j++, c += 4) {
-							data[i][j] = [imageData.data[c], imageData.data[c + 1], imageData.data[c + 2], imageData.data[c + 3]]
+							data[i][j] = [
+								imageData.data[c],
+								imageData.data[c + 1],
+								imageData.data[c + 2],
+								imageData.data[c + 3],
+							]
 						}
 					}
 					cb(data)
 				}
 			}
 		} else if (data instanceof HTMLImageElement || data instanceof HTMLVideoElement) {
-			const canvas = document.createElement("canvas")
+			const canvas = document.createElement('canvas')
 			canvas.width = this._video.videoWidth
 			canvas.height = this._video.videoHeight
 			const context = canvas.getContext('2d')
@@ -115,13 +120,13 @@ export default class ImageData extends BaseData {
 		}
 	}
 
-	_reduce(im, step, algorithm = "mean") {
+	_reduce(im, step, algorithm = 'mean') {
 		const x = []
 		const d = im[0][0].length
 		let f = null
-		if (algorithm === "max") {
+		if (algorithm === 'max') {
 			f = Math.max
-		} else if (algorithm === "mean") {
+		} else if (algorithm === 'mean') {
 			f = (a, b) => a + b
 		}
 		for (let i = 0, p = 0; i < im.length; i += step, p++) {
@@ -142,7 +147,7 @@ export default class ImageData extends BaseData {
 					}
 				}
 				x[p][q] = m
-				if (algorithm === "mean") {
+				if (algorithm === 'mean') {
 					x[p][q] = m.map(v => v / (step * step))
 				}
 			}
@@ -152,13 +157,13 @@ export default class ImageData extends BaseData {
 
 	_convertSpace(data, space, normalize = false, binary_threshold = 180) {
 		const [r, g, b, a] = data
-		if (space === "rgb") {
+		if (space === 'rgb') {
 			if (normalize) {
 				return [r / 255, g / 255, b / 255]
 			} else {
 				return [r, g, b]
 			}
-		} else if (space === "8 colors") {
+		} else if (space === '8 colors') {
 			const br = r >> 7 ? 255 : 0
 			const bg = g >> 7 ? 255 : 0
 			const bb = b >> 7 ? 255 : 0
@@ -167,14 +172,14 @@ export default class ImageData extends BaseData {
 			} else {
 				return [br, bg, bb]
 			}
-		} else if (space === "gray") {
+		} else if (space === 'gray') {
 			const v = ImageData.grayScales.BT709.calc(r, g, b)
 			if (normalize) {
 				return [v / 255]
 			} else {
 				return [v]
 			}
-		} else if (space === "binary") {
+		} else if (space === 'binary') {
 			let v = ImageData.grayScales.BT709.calc(r, g, b)
 			v = v < binary_threshold ? 0 : 255
 			if (normalize) {
@@ -182,17 +187,17 @@ export default class ImageData extends BaseData {
 			} else {
 				return [v]
 			}
-		} else if (space === "hls") {
+		} else if (space === 'hls') {
 			const max = Math.max(r, g, b)
 			const min = Math.min(r, g, b)
 			let h = null
 			if (max !== min) {
 				if (min === b) {
-					h = 60 * (g - r) / (max - min) + 60
+					h = (60 * (g - r)) / (max - min) + 60
 				} else if (min === r) {
-					h = 60 * (b - g) / (max - min) + 180
+					h = (60 * (b - g)) / (max - min) + 180
 				} else if (min === g) {
-					h = 60 * (r - b) / (max - min) + 300
+					h = (60 * (r - b)) / (max - min) + 300
 				}
 			}
 			const l = (max + min) / 2
@@ -202,17 +207,17 @@ export default class ImageData extends BaseData {
 			} else {
 				return [h, l, s]
 			}
-		} else if (space === "hsv") {
+		} else if (space === 'hsv') {
 			const max = Math.max(r, g, b)
 			const min = Math.min(r, g, b)
 			let h = null
 			if (max !== min) {
 				if (min === b) {
-					h = 60 * (g - r) / (max - min) + 60
+					h = (60 * (g - r)) / (max - min) + 60
 				} else if (min === r) {
-					h = 60 * (b - g) / (max - min) + 180
+					h = (60 * (b - g)) / (max - min) + 180
 				} else if (min === g) {
-					h = 60 * (r - b) / (max - min) + 300
+					h = (60 * (r - b)) / (max - min) + 300
 				}
 			}
 			const l = max
@@ -235,5 +240,38 @@ export default class ImageData extends BaseData {
 		}
 		return cp
 	}
-}
 
+	_createCanvas(data, width = 80, height = null) {
+		const orgwidth = data[0].length
+		const orgheight = data.length
+		if (!height) {
+			height = Math.ceil((width * orgheight) / orgwidth)
+		}
+		const canvas = document.createElement('canvas')
+		canvas.width = orgwidth
+		canvas.height = orgheight
+		const context = canvas.getContext('2d')
+		const imdata = context.createImageData(canvas.width, canvas.height)
+		for (let i = 0, c = 0; i < canvas.height; i++) {
+			for (let j = 0; j < canvas.width; j++, c += 4) {
+				imdata.data[c] = data[i][j][0]
+				if (data[i][j].length === 1) {
+					imdata.data[c + 1] = data[i][j][0]
+					imdata.data[c + 2] = data[i][j][0]
+					imdata.data[c + 3] = 255
+				} else if (data[i][j].length >= 3) {
+					imdata.data[c + 1] = data[i][j][1]
+					imdata.data[c + 2] = data[i][j][2]
+					imdata.data[c + 3] = data[i][j][3] || 255
+				}
+			}
+		}
+		context.putImageData(imdata, 0, 0)
+		const tcanvas = document.createElement('canvas')
+		tcanvas.width = width
+		tcanvas.height = height
+		const tcontext = tcanvas.getContext('2d')
+		tcontext.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, width, height)
+		return tcanvas
+	}
+}
