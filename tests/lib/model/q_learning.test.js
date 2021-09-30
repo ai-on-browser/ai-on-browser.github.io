4b4fefca9ad2dad4d7f53ae51fa33941ae82247d
ishii-norimi <mirasunimoni@yahoo.co.jp>
2021-09-30 21:42:31 +0900

    Add test

---
+++ tests/lib/model/q_learning.test.js
@@ -0,0 +1,24 @@
+import QAgent from '../../../lib/model/q_learning.js'
+import GridRLEnvironment from '../../../lib/rl/grid.js'
+
+test('default', () => {
+	const env = new GridRLEnvironment()
+	const agent = new QAgent(env, env.size[0])
+
+    let totalReward = 0
+    const n = 1000
+	for (let i = 0; i < n; i++) {
+		let curState = env.reset()
+		while (true) {
+			const action = agent.get_action(env, curState, 0.01)
+			const {state, reward, done} = env.step(action)
+			agent.update(action, curState, state, reward)
+            totalReward += reward
+			curState = state
+			if (done) {
+				break
+			}
+		}
+	}
+    expect(totalReward / n).toBeGreaterThan(-60)
+})
