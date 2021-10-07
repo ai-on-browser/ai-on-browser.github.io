b9908d04f278baf5772da8c408b6c7f2a3f696af
ishii-norimi <mirasunimoni@yahoo.co.jp>
2021-10-07 22:13:59 +0900

    Add and improve some neuralnetwork layers

--- tests/lib/model/layer/onehot.test.js
+++ tests/lib/model/layer/onehot.test.js
@@ -22,29 +22,4 @@ describe('onehot', () => {
 			}
 		}
 	})
-
-	test('grad', () => {
-		const net = new NeuralNetwork(
-			[{ type: 'input' }, { type: 'full', out_size: 1 }, { type: 'onehot' }],
-			'mse',
-			'adam'
-		)
-		const x = Matrix.random(5, 3, -0.1, 0.1)
-		const t = Matrix.zeros(5, 5)
-		for (let i = 0; i < 5; i++) {
-			t.set(i, i, 1)
-		}
-
-		for (let i = 0; i < 100; i++) {
-			const loss = net.fit(x, t, 1000, 0.01)
-			if (loss[0] < 1.0e-8) {
-				break
-			}
-		}
-
-		const y = net.calc(x)
-		for (let i = 0; i < t.cols; i++) {
-			expect(y.at(0, i)).toBeCloseTo(t.at(0, i))
-		}
-	})
 })
