49b61f9d7fe813112680d66758338351714e911c
ishii-norimi <mirasunimoni@yahoo.co.jp>
2021-10-08 23:31:16 +0900

    Add perceptron test

--- tests/lib/model/perceptron.test.js
+++ tests/lib/model/perceptron.test.js
@@ -5,25 +5,27 @@ test('default', () => {
 	expect(model._r).toBe(0.1)
 })
 
-test.each([
-	[1, 1, -1, -1],
-	[1, -1, 1, -1],
-	[-1, -1, 1, 1],
-])('fit[%i, %i, %i, %i]', (a, b, c, d) => {
-	const model = new Perceptron(false, 0.1)
-	const x = [
-		[1, 1],
-		[1, 0],
-		[0, 1],
-		[0, 0],
-	]
-	const t = [[a], [b], [c], [d]]
-	model.init(x, t)
-	for (let i = 0; i < 1000; i++) {
-		model.fit()
-	}
-	const y = model.predict(x)
-	for (let i = 0; i < 4; i++) {
-		expect(y[i]).toBeCloseTo(t[i][0])
-	}
+describe.each([undefined, false, true])('average %p', average => {
+	test.each([
+		[1, 1, -1, -1],
+		[1, -1, 1, -1],
+		[-1, -1, 1, 1],
+	])('fit[%i, %i, %i, %i]', (a, b, c, d) => {
+		const model = new Perceptron(average, 0.1)
+		const x = [
+			[1, 1],
+			[1, 0],
+			[0, 1],
+			[0, 0],
+		]
+		const t = [[a], [b], [c], [d]]
+		model.init(x, t)
+		for (let i = 0; i < 1000; i++) {
+			model.fit()
+		}
+		const y = model.predict(x)
+		for (let i = 0; i < 4; i++) {
+			expect(y[i]).toBeCloseTo(t[i][0])
+		}
+	})
 })
