2de7e871b1f28101cd79f3c34c2ff0ab705317ba
ishii-norimi <mirasunimoni@yahoo.co.jp>
2021-10-20 23:05:46 +0900

    Improve Matrix class and add tests

--- tests/lib/model/q_learning.test.js
+++ tests/lib/model/q_learning.test.js
@@ -5,22 +5,22 @@ test('default', () => {
 	const env = new GridRLEnvironment()
 	const agent = new QAgent(env, env.size[0])
 
-    let totalReward = 0
-    const n = 1000
+	let totalReward = 0
+	const n = 1000
 	for (let i = 0; i < n; i++) {
 		let curState = env.reset()
 		while (true) {
 			const action = agent.get_action(env, curState, 0.01)
-			const {state, reward, done} = env.step(action)
+			const { state, reward, done } = env.step(action)
 			agent.update(action, curState, state, reward)
-            totalReward += reward
+			totalReward += reward
 			curState = state
 			if (done) {
 				break
 			}
 		}
 	}
-    expect(totalReward / n).toBeGreaterThan(-60)
+	expect(totalReward / n).toBeGreaterThan(-60)
 	const score = agent.get_score(env)
 	expect(score).toHaveLength(20)
 	expect(score[0]).toHaveLength(10)
