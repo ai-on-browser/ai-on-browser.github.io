a1be2f5a90f1648d14fb44e024d287195842d4a3
ishii-norimi <mirasunimoni@yahoo.co.jp>
2021-11-23 11:58:16 +0900

    Add dual PCA

--- js/view/pca.js
+++ js/view/pca.js
@@ -1,14 +1,26 @@
-import { PCA, AnomalyPCA } from '../../lib/model/pca.js'
+import { PCA, DualPCA, KernelPCA, AnomalyPCA } from '../../lib/model/pca.js'
 
 var dispPCA = function (elm, platform) {
-	let poly_dimension = 2
-
 	const fitModel = () => {
 		platform.fit((tx, ty, pred_cb) => {
 			if (platform.task === 'DR') {
 				const dim = platform.dimension
 				const kernel = elm.select('[name=kernel]').property('value')
-				const model = new PCA(kernel, [poly_dimension])
+				const type = elm.select('[name=type]').property('value')
+				let model
+				if (type === '') {
+					model = new PCA()
+				} else if (type === 'dual') {
+					model = new DualPCA()
+				} else {
+					const args = []
+					if (kernel === 'polynomial') {
+						args.push(+elm.select('[name=poly_d]').property('value'))
+					} else if (kernel === 'gaussian') {
+						args.push(+elm.select('[name=sigma]').property('value'))
+					}
+					model = new KernelPCA(kernel, args)
+				}
 				model.fit(tx)
 				const y = model.predict(tx, dim)
 				pred_cb(y)
@@ -28,38 +40,63 @@ var dispPCA = function (elm, platform) {
 
 	if (platform.task !== 'AD') {
 		elm.append('select')
-			.attr('name', 'kernel')
+			.attr('name', 'type')
 			.on('change', function () {
 				const slct = d3.select(this)
-				if (slct.property('value') === 'polynomial') {
-					elm.select('[name=poly_dimension]').style('display', 'inline-block')
+				if (slct.property('value') === 'kernel') {
+					kernelElm.style('display', 'inline-block')
 				} else {
-					elm.select('[name=poly_dimension]').style('display', 'none')
+					kernelElm.style('display', 'none')
 				}
 			})
 			.selectAll('option')
-			.data(['no kernel', 'gaussian', 'polynomial'])
+			.data(['', 'dual', 'kernel'])
 			.enter()
 			.append('option')
 			.attr('value', d => d)
 			.text(d => d)
 	}
-	elm.append('span')
-		.attr('name', 'poly_dimension')
-		.style('display', 'none')
-		.each(function () {
-			d3.select(this)
-				.append('span')
-				.text(' d = ')
-				.append('input')
-				.attr('type', 'number')
-				.attr('value', 2)
-				.attr('min', 1)
-				.attr('max', 10)
-				.on('change', function () {
-					poly_dimension = d3.select(this).property('value')
-				})
+	const kernelElm = elm.append('span').style('display', 'none')
+	kernelElm
+		.append('select')
+		.attr('name', 'kernel')
+		.on('change', function () {
+			const slct = d3.select(this)
+			poly_dimension.style('display', 'none')
+			gauss_sigma.style('display', 'none')
+			if (slct.property('value') === 'polynomial') {
+				poly_dimension.style('display', 'inline-block')
+			} else if (slct.property('value') === 'gaussian') {
+				gauss_sigma.style('display', 'inline-block')
+			}
 		})
+		.selectAll('option')
+		.data(['gaussian', 'polynomial'])
+		.enter()
+		.append('option')
+		.attr('value', d => d)
+		.text(d => d)
+	const poly_dimension = kernelElm.append('span').style('display', 'none')
+	poly_dimension
+		.append('span')
+		.text(' d = ')
+		.append('input')
+		.attr('type', 'number')
+		.attr('name', 'poly_d')
+		.attr('value', 2)
+		.attr('min', 1)
+		.attr('max', 10)
+	const gauss_sigma = kernelElm.append('span')
+	gauss_sigma
+		.append('span')
+		.text(' sigma = ')
+		.append('input')
+		.attr('type', 'number')
+		.attr('name', 'sigma')
+		.attr('value', 1)
+		.attr('min', 0)
+		.attr('max', 10)
+		.attr('step', 0.1)
 	if (platform.task === 'AD') {
 		elm.append('span').text(' threshold = ')
 		elm.append('input')
