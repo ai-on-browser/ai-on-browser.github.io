2f8f0fa06515a590ffc7ae9af0981e0f6e9dfb2a
ishii-norimi <mirasunimoni@yahoo.co.jp>
2021-12-14 22:43:23 +0900

    Add Self-training and improve semi-supervised models

--- tests/lib/model/gmm.test.js
+++ tests/lib/model/gmm.test.js
@@ -2,7 +2,7 @@ import { jest } from '@jest/globals'
 jest.retryTimes(5)
 
 import { Matrix } from '../../../lib/util/math.js'
-import { GMM, GMR } from '../../../lib/model/gmm.js'
+import { GMM, GMR, SemiSupervisedGMM } from '../../../lib/model/gmm.js'
 
 test('clustering', () => {
 	const model = new GMM()
@@ -62,4 +62,29 @@ test('regression', () => {
 		err += (y[i][0] - t[i][0]) ** 2
 	}
 	expect(Math.sqrt(err / t.length)).toBeLessThan(0.5)
+})
+
+test('semi-classifier', () => {
+	const model = new SemiSupervisedGMM()
+	const x = Matrix.randn(50, 2, 0, 0.2).concat(Matrix.randn(50, 2, 5, 0.2)).toArray()
+	const t = []
+	const t_org = []
+	for (let i = 0; i < x.length; i++) {
+		t_org[i] = t[i] = Math.floor(i / 50) * 2 - 1
+		if (Math.random() < 0.5) {
+			t[i] = null
+		}
+	}
+	model.init(x, t)
+	for (let i = 0; i < 20; i++) {
+		model.fit(x, t)
+	}
+	const y = model.predict(x)
+	let acc = 0
+	for (let i = 0; i < t.length; i++) {
+		if (y[i] === t_org[i]) {
+			acc++
+		}
+	}
+	expect(acc / y.length).toBeGreaterThan(0.95)
 })
--- tests/lib/model/kmeans.test.js
+++ tests/lib/model/kmeans.test.js
@@ -1,12 +1,17 @@
 import { Matrix } from '../../../lib/util/math.js'
-import { KMeansModel, KMeans, KMeanspp, KMedoids, KMedians } from '../../../lib/model/kmeans.js'
+import {
+	KMeansModel,
+	KMeans,
+	KMeanspp,
+	KMedoids,
+	KMedians,
+	SemiSupervisedKMeansModel,
+} from '../../../lib/model/kmeans.js'
 
 test.each([undefined, KMeans, KMeanspp, KMedoids, KMedians])('predict %p', methodCls => {
 	const model = new KMeansModel(methodCls ? new methodCls() : undefined)
 	const n = 50
-	const x = Matrix.randn(n, 2, 0, 0.1)
-		.concat(Matrix.randn(n, 2, 5, 0.1))
-		.toArray()
+	const x = Matrix.randn(n, 2, 0, 0.1).concat(Matrix.randn(n, 2, 5, 0.1)).toArray()
 
 	model.add(x)
 	model.add(x)
@@ -39,4 +44,29 @@ test.each([undefined, KMeans, KMeanspp, KMedoids, KMedians])('predict %p', metho
 		}
 	}
 	expect(acc / y.length).toBeGreaterThan(0.9)
+})
+
+test('semi-classifier', () => {
+	const model = new SemiSupervisedKMeansModel()
+	const x = Matrix.randn(50, 2, 0, 0.2).concat(Matrix.randn(50, 2, 5, 0.2)).toArray()
+	const t = []
+	const t_org = []
+	for (let i = 0; i < x.length; i++) {
+		t_org[i] = t[i] = Math.floor(i / 50) * 2 - 1
+		if (Math.random() < 0.5) {
+			t[i] = null
+		}
+	}
+	model.init(x, t)
+	for (let i = 0; i < 20; i++) {
+		model.fit(x, t)
+	}
+	const y = model.predict(x)
+	let acc = 0
+	for (let i = 0; i < t.length; i++) {
+		if (y[i] === t_org[i]) {
+			acc++
+		}
+	}
+	expect(acc / y.length).toBeGreaterThan(0.95)
 })
