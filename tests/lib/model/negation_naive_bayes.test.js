e670ec572e6e29d3666d420ba33717610554610e
ishii-norimi <ishii-norimi@users.noreply.github.com>
2021-12-16 10:25:20 +0000

    deploy: 01f715632c63d9c302ae1839eaa83459df0bda1e

---
+++ tests/lib/model/negation_naive_bayes.test.js
@@ -0,0 +1,22 @@
+import { Matrix } from '../../../lib/util/math.js'
+import { GaussianNegationNaiveBayes } from '../../../lib/model/negation_naive_bayes.js'
+
+test('predict', () => {
+	const model = new GaussianNegationNaiveBayes()
+	const x = Matrix.randn(50, 2, 0, 0.2).concat(Matrix.randn(50, 2, 5, 0.2)).toArray()
+	const t = []
+	for (let i = 0; i < x.length; i++) {
+		t[i] = Math.floor(i / 50)
+	}
+
+	model.fit(x, t)
+	const y = model.predict(x)
+	expect(y).toHaveLength(x.length)
+	let acc = 0
+	for (let i = 0; i < t.length; i++) {
+		if (y[i] === t[i]) {
+			acc++
+		}
+	}
+	expect(acc / y.length).toBeGreaterThan(0.95)
+})
