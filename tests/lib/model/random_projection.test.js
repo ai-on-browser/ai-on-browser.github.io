6567fe34f9df6b0be12f17c93c0fd7e58069e670
ishii-norimi <mirasunimoni@yahoo.co.jp>
2021-12-21 00:08:04 +0900

    Add test of dimensionality reduction models

---
+++ tests/lib/model/random_projection.test.js
@@ -0,0 +1,11 @@
+import { Matrix } from '../../../lib/util/math.js'
+import RandomProjection from '../../../lib/model/random_projection.js'
+
+import { coRankingMatrix } from '../../../lib/evaluate/dimensionality_reduction.js'
+
+test('dimensionality reduction', () => {
+	const x = Matrix.randn(50, 5, 0, 0.2).concat(Matrix.randn(50, 5, 5, 0.2)).toArray()
+	const y = RandomProjection(x, 2)
+	const q = coRankingMatrix(x, y, 30, 20)
+	expect(q).toBeGreaterThan(0.9)
+})
