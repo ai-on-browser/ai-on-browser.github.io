51e03bb4c7147aea7d650d1d780a54e734dd397a
ishii-norimi <mirasunimoni@yahoo.co.jp>
2021-12-23 21:25:02 +0900

    Add some tests and fix jsdoc

---
+++ tests/lib/model/kernel_density_estimator.test.js
@@ -0,0 +1,26 @@
+import { jest } from '@jest/globals'
+jest.retryTimes(3)
+
+import { Matrix } from '../../../lib/util/math.js'
+import KernelDensityEstimator from '../../../lib/model/kernel_density_estimator.js'
+
+import { correlation } from '../../../lib/evaluate/regression.js'
+
+test('density estimation', () => {
+	const model = new KernelDensityEstimator()
+	const n = 500
+	const x = Matrix.randn(n, 2, 0, 0.1).concat(Matrix.randn(n, 2, 5, 0.1)).toArray()
+
+	model.fit(x)
+	const y = model.predict(x)
+	expect(y).toHaveLength(x.length)
+
+	const p = []
+	for (let i = 0; i < x.length; i++) {
+		const p1 = Math.exp(-x[i].reduce((s, v) => s + v ** 2, 0) / (2 * 0.1)) / Math.sqrt(2 * Math.PI * 0.1)
+		const p2 = Math.exp(-x[i].reduce((s, v) => s + (v - 5) ** 2, 0) / (2 * 0.1)) / Math.sqrt(2 * Math.PI * 0.1)
+		p[i] = (p1 + p2) / 2
+	}
+	const corr = correlation(y, p)
+	expect(corr).toBeGreaterThan(0.9)
+})
---
+++ tests/lib/model/rbm.test.js
@@ -0,0 +1,32 @@
+import { jest } from '@jest/globals'
+jest.retryTimes(3)
+
+import { Matrix } from '../../../lib/util/math.js'
+import { RBM, GBRBM } from '../../../lib/model/rbm.js'
+
+test('reconstruct RBM', () => {
+	const model = new RBM(10)
+	const x = [
+		[1, 1, 1, 0, 0, 0],
+		[0, 0, 0, 1, 1, 1],
+	]
+	for (let i = 0; i < 10000; i++) {
+		model.fit(x)
+	}
+	const y = model.predict([
+		[1, 0, 1, 0, 0, 0],
+		[0, 1, 0, 1, 1, 1],
+	])
+	expect(y).toEqual(x)
+})
+
+test('reconstruct GBRBM', () => {
+	const model = new GBRBM(10)
+	const x = Matrix.randn(200, 3, 1, 0.3).toArray()
+	for (let i = 0; i < 1000; i++) {
+		model.fit(x)
+	}
+	const y = Matrix.fromArray(model.predict(x))
+	expect(y.mean()).toBeCloseTo(1, 1)
+	expect(y.variance()).toBeCloseTo(0.3, 1)
+})
