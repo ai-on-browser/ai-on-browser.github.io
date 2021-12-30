d9ce8474236a2fe3e084d8a1d616a35c144f022e
ishii-norimi <ishii-norimi@users.noreply.github.com>
2021-12-30 14:48:45 +0000

    deploy: 9728017c4038d0120c0ee3699fa9f8a627229fd0

---
+++ tests/lib/model/layer/acosh.test.js
@@ -0,0 +1,38 @@
+import NeuralNetwork from '../../../../lib/model/neuralnetwork.js'
+import { Matrix } from '../../../../lib/util/math.js'
+
+describe('acosh', () => {
+	test('calc', () => {
+		const net = NeuralNetwork.fromObject([{ type: 'input' }, { type: 'acosh' }])
+		const x = Matrix.random(10, 10, 1, 5)
+
+		const y = net.calc(x)
+		for (let i = 0; i < x.rows; i++) {
+			for (let j = 0; j < x.cols; j++) {
+				expect(y.at(i, j)).toBeCloseTo(Math.acosh(x.at(i, j)))
+			}
+		}
+	})
+
+	test('grad', () => {
+		const net = NeuralNetwork.fromObject(
+			[{ type: 'input' }, { type: 'full', out_size: 3 }, { type: 'clip', min: 1, max: 10 }, { type: 'acosh' }],
+			'mse',
+			'adam'
+		)
+		const x = Matrix.random(1, 5, -0.1, 0.1)
+		const t = Matrix.random(1, 3, 0.1, 2)
+
+		for (let i = 0; i < 100; i++) {
+			const loss = net.fit(x, t, 1000, 0.01)
+			if (loss[0] < 1.0e-8) {
+				break
+			}
+		}
+
+		const y = net.calc(x)
+		for (let i = 0; i < t.cols; i++) {
+			expect(y.at(0, i)).toBeCloseTo(t.at(0, i))
+		}
+	})
+})
