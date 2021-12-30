0ada8738d586c87e401d2dbebe3e99f81f414935
ishii-norimi <mirasunimoni@yahoo.co.jp>
2021-12-30 23:57:26 +0900

    Add test and update README.md

---
+++ tests/lib/model/nmf.test.js
@@ -0,0 +1,17 @@
+import { Matrix } from '../../../lib/util/math.js'
+import NMF from '../../../lib/model/nmf.js'
+
+import { coRankingMatrix } from '../../../lib/evaluate/dimensionality_reduction.js'
+
+test('dimensionality reduction', () => {
+	const x = Matrix.random(50, 5, 0, 2).concat(Matrix.random(50, 5, 5, 8)).toArray()
+	const model = new NMF()
+
+	model.init(x, 2)
+	for (let i = 0; i < 100; i++) {
+		model.fit()
+	}
+	const y = model.predict()
+	const q = coRankingMatrix(x, y, 30, 20)
+	expect(q).toBeGreaterThan(0.9)
+})
