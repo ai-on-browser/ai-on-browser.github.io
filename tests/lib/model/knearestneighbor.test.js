cb1fd882e1a0bff880e6cd2c423934dd22673093
ishii-norimi <mirasunimoni@yahoo.co.jp>
2022-01-05 21:38:45 +0900

    Fix kNN method

--- tests/lib/model/knearestneighbor.test.js
+++ tests/lib/model/knearestneighbor.test.js
@@ -1,8 +1,17 @@
-import { KNN, KNNRegression, SemiSupervisedKNN, KNNAnomaly } from '../../../lib/model/knearestneighbor.js'
+import { jest } from '@jest/globals'
+jest.retryTimes(3)
+
+import {
+	KNN,
+	KNNRegression,
+	SemiSupervisedKNN,
+	KNNAnomaly,
+	KNNDensityEstimation,
+} from '../../../lib/model/knearestneighbor.js'
 import { Matrix } from '../../../lib/util/math.js'
 
 import { accuracy } from '../../../lib/evaluate/classification.js'
-import { rmse } from '../../../lib/evaluate/regression.js'
+import { rmse, correlation } from '../../../lib/evaluate/regression.js'
 
 test.each(['euclid', 'manhattan', 'chebyshev'])('classifier %s', metric => {
 	const model = new KNN(5, metric)
@@ -58,4 +67,23 @@ test.each(['euclid', 'manhattan', 'chebyshev'])('anomaly detection %s', metric =
 		expect(y[i]).toBe(false)
 	}
 	expect(y[y.length - 1]).toBe(true)
+})
+
+test.each(['euclid', 'manhattan', 'chebyshev'])('density estimation %s', metric => {
+	const model = new KNNDensityEstimation(50, metric)
+	const n = 100
+	const x = Matrix.randn(n, 2, 0, 0.1).concat(Matrix.randn(n, 2, 5, 0.1)).toArray()
+
+	model.fit(x)
+	const y = model.predict(x)
+	expect(y).toHaveLength(x.length)
+
+	const p = []
+	for (let i = 0; i < x.length; i++) {
+		const p1 = Math.exp(-x[i].reduce((s, v) => s + v ** 2, 0) / (2 * 0.1)) / (2 * Math.PI * 0.1)
+		const p2 = Math.exp(-x[i].reduce((s, v) => s + (v - 5) ** 2, 0) / (2 * 0.1)) / (2 * Math.PI * 0.1)
+		p[i] = (p1 + p2) / 2
+	}
+	const corr = correlation(y, p)
+	expect(corr).toBeGreaterThan(0.9)
 })
