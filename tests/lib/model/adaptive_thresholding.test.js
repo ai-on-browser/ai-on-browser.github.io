c97f187f431da7d7ebee415eec50fc1afcd86967
ishii-norimi <mirasunimoni@yahoo.co.jp>
2022-01-12 22:50:05 +0900

    Fix test

--- tests/lib/model/adaptive_thresholding.test.js
+++ tests/lib/model/adaptive_thresholding.test.js
@@ -8,17 +8,14 @@ test('default', () => {
 
 test.each(['mean', 'gaussian', 'median', 'midgray'])('predict', method => {
 	const model = new AdaptiveThresholding(method)
-	const x = Tensor.random([10, 10, 3]).toArray()
+	const x = Tensor.random([10, 10]).toArray()
 
 	const y = model.predict(x)
 	expect(y).toHaveLength(10)
 	expect(y[0]).toHaveLength(10)
-	expect(y[0][0]).toHaveLength(3)
 	for (let i = 0; i < 10; i++) {
 		for (let j = 0; j < 10; j++) {
-			for (let k = 0; k < 3; k++) {
-				expect(y[i][j][k] === 0 || y[i][j][k] === 255).toBeTruthy()
-			}
+			expect(y[i][j] === 0 || y[i][j] === 255).toBeTruthy()
 		}
 	}
 })
