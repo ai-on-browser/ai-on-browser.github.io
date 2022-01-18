02f50d483459506a5807596ffb94a8d8166116e9
ishii-norimi <mirasunimoni@yahoo.co.jp>
2022-01-18 23:23:38 +0900

    Add test and improve bit

---
+++ tests/lib/model/ica.test.js
@@ -0,0 +1,16 @@
+import { Matrix } from '../../../lib/util/math.js'
+import ICA from '../../../lib/model/ica.js'
+
+import { coRankingMatrix } from '../../../lib/evaluate/dimensionality_reduction.js'
+
+test('dimensionality reduction', () => {
+	const x = Matrix.randn(50, 5, 0, Matrix.randn(5, 5).gram().toArray())
+		.concat(Matrix.randn(50, 5, 5, Matrix.randn(5, 5).gram().toArray()))
+		.toArray()
+	const model = new ICA()
+	model.fit(x)
+	const y = model.predict(x, 2)
+
+	const q = coRankingMatrix(x, y, 30, 20)
+	expect(q).toBeGreaterThan(0.9)
+})
