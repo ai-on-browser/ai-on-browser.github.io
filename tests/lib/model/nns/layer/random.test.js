42581798e263d8f1c9d8be4e80bc46646bbe3c7f
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-01-29 19:17:03 +0900

    Change random layer to be able to get sizes from other layer (#4)

--- tests/lib/model/nns/layer/random.test.js
+++ tests/lib/model/nns/layer/random.test.js
@@ -1,4 +1,5 @@
 import NeuralNetwork from '../../../../../lib/model/neuralnetwork.js'
+import Matrix from '../../../../../lib/util/matrix.js'
 
 describe('random', () => {
 	test('scalar', () => {
@@ -10,5 +11,16 @@ describe('random', () => {
 		for (let i = 0; i < y1.cols; i++) {
 			expect(y1.at(0, i)).not.toBe(y2.at(0, i))
 		}
+	})
+
+	test('name', () => {
+		const net = NeuralNetwork.fromObject([
+			{ type: 'input', name: 'x' },
+			{ type: 'random', size: 'x' },
+		])
+		const x = Matrix.ones(10, 3)
+
+		const y = net.calc(x)
+		expect(y.sizes).toEqual(x.sizes)
 	})
 })
