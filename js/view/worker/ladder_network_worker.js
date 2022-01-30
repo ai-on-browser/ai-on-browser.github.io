52cdc02cde25bcc04a87cbb85413eb9e79fc5627
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-01-30 20:46:01 +0900

    Add Ladder network (#8)
    * Add std layer and improve some layers
    
    * Add Ladder network
    
    * Improve test
    
    * Improve test

---
+++ js/view/worker/ladder_network_worker.js
@@ -0,0 +1,27 @@
+import LadderNetwork from '../../../lib/model/ladder_network.js'
+
+self.model = null
+
+self.addEventListener(
+	'message',
+	function (e) {
+		const data = e.data
+		if (data.mode === 'init') {
+			self.model = new LadderNetwork(data.hidden_sizes, data.lambdas, data.activation, data.optimizer)
+			self.postMessage(null)
+		} else if (data.mode === 'fit') {
+			const samples = data.x.length
+			if (samples === 0) {
+				self.postMessage(null)
+				return
+			}
+
+			self.model.fit(data.x, data.y, data.iteration, data.rate, data.batch)
+			self.postMessage({ epoch: self.model.epoch })
+		} else if (data.mode === 'predict') {
+			const pred = self.model.predict(data.x)
+			self.postMessage(pred)
+		}
+	},
+	false
+)
---
+++ tests/lib/model/ladder_network.test.js
@@ -0,0 +1,26 @@
+import { jest } from '@jest/globals'
+jest.retryTimes(3)
+
+import Matrix from '../../../lib/util/matrix.js'
+import LadderNetwork from '../../../lib/model/ladder_network.js'
+
+import { accuracy } from '../../../lib/evaluate/classification.js'
+
+test('semi-classifier', () => {
+	const model = new LadderNetwork([5], [0.001, 0.0001, 0.0001], 'tanh', 'adam')
+	const x = Matrix.randn(50, 2, 0, 0.2).concat(Matrix.randn(50, 2, 5, 0.2)).toArray()
+	const t = []
+	const t_org = []
+	for (let i = 0; i < x.length; i++) {
+		t_org[i] = t[i] = String.fromCharCode('a'.charCodeAt(0) + Math.floor(i / 50))
+		if (Math.random() < 0.5) {
+			t[i] = null
+		}
+	}
+	for (let i = 0; i < 5; i++) {
+		model.fit(x, t, 100, 0.001)
+	}
+	const y = model.predict(x)
+	const acc = accuracy(y, t_org)
+	expect(acc).toBeGreaterThan(0.95)
+})
