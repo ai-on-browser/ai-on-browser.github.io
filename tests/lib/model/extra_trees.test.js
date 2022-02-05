b20d19b0a5df5b8f47e218886a62f3953bd50418
ishii-norimi <mirasunimoni@yahoo.co.jp>
2022-02-05 14:07:09 +0900

    Add extra trees

---
+++ tests/lib/model/extra_trees.test.js
@@ -0,0 +1,38 @@
+import { ExtraTreesClassifier, ExtraTreesRegressor } from '../../../lib/model/extra_trees.js'
+import Matrix from '../../../lib/util/matrix.js'
+
+import { accuracy } from '../../../lib/evaluate/classification.js'
+import { rmse } from '../../../lib/evaluate/regression.js'
+
+test('classifier', () => {
+	const model = new ExtraTreesClassifier(100)
+	const x = Matrix.randn(50, 2, 0, 0.2).concat(Matrix.randn(50, 2, 5, 0.2)).toArray()
+	const t = []
+	for (let i = 0; i < x.length; i++) {
+		t[i] = String.fromCharCode('a'.charCodeAt(0) + Math.floor(i / 50))
+	}
+	model.init(x, t)
+	for (let i = 0; i < 100; i++) {
+		model.fit()
+	}
+	const y = model.predict(x)
+	expect(y).toHaveLength(x.length)
+	const acc = accuracy(y, t)
+	expect(acc).toBeGreaterThan(0.95)
+})
+
+test('regression', () => {
+	const model = new ExtraTreesRegressor(100)
+	const x = Matrix.random(50, 10, -2, 2).toArray()
+	const t = []
+	for (let i = 0; i < x.length; i++) {
+		t[i] = x[i][0] + x[i][1] + (Math.random() - 0.5) / 10
+	}
+	model.init(x, t)
+	for (let i = 0; i < 100; i++) {
+		model.fit()
+	}
+	const y = model.predict(x)
+	const err = rmse(y, t)
+	expect(err).toBeLessThan(0.5)
+})
