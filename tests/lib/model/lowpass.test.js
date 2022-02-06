128fd89c48a27348c63b9f5a98996154ed9a9ec4
ishii-norimi <mirasunimoni@yahoo.co.jp>
2022-02-06 12:30:23 +0900

    Use DFT in lowpass filter

--- tests/lib/model/lowpass.test.js
+++ tests/lib/model/lowpass.test.js
@@ -5,10 +5,24 @@ import { LowpassFilter } from '../../../lib/model/lowpass.js'
 
 import { rmse } from '../../../lib/evaluate/regression.js'
 
-test('smoothing', () => {
+test('smoothing dft', () => {
 	const x = []
 	const t = []
 	for (let i = 0; i < 100; i++) {
+		x[i] = Math.sin(i / 20) + (Math.random() - 0.5) / 2
+		t[i] = Math.sin(i / 20)
+	}
+	const model = new LowpassFilter(0.8)
+	const y = model.predict(x)
+	expect(y).toHaveLength(t.length)
+	const err = rmse(y, t)
+	expect(err).toBeLessThan(rmse(x, t))
+})
+
+test('smoothing fft', () => {
+	const x = []
+	const t = []
+	for (let i = 0; i < 128; i++) {
 		x[i] = Math.sin(i / 20) + (Math.random() - 0.5) / 2
 		t[i] = Math.sin(i / 20)
 	}
