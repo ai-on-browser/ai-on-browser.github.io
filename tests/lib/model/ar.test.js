dd2113f3913b98ee33328db823c417f0c9eb95f4
ishii-norimi <mirasunimoni@yahoo.co.jp>
2022-02-08 00:03:34 +0900

    Make it possible to choose the method of AR model

--- tests/lib/model/ar.test.js
+++ tests/lib/model/ar.test.js
@@ -3,8 +3,8 @@ jest.retryTimes(3)
 
 import AR from '../../../lib/model/ar.js'
 
-test('linear', () => {
-	const model = new AR(5)
+test.each([undefined, 'lsm', 'householder'])('linear %s', method => {
+	const model = new AR(5, method)
 	const x = []
 	for (let i = 0; i < 100; i++) {
 		x[i] = i / 10 + (Math.random() - 0.5) / 100
@@ -18,8 +18,8 @@ test('linear', () => {
 	}
 })
 
-test('sin', () => {
-	const model = new AR(10)
+test.each([undefined, 'lsm', 'householder'])('sin %s', method => {
+	const model = new AR(10, method)
 	const x = []
 	for (let i = 0; i < 100; i++) {
 		x[i] = Math.sin(i / 10) + (Math.random() - 0.5) / 100
@@ -30,5 +30,20 @@ test('sin', () => {
 	expect(future).toHaveLength(50)
 	for (let i = 0; i < 50; i++) {
 		expect(future[i]).toBeCloseTo(Math.sin(10 + i / 10), 1)
+	}
+})
+
+test.each(['yuleWalker', 'levinson'])('sin %s', method => {
+	const model = new AR(10, method)
+	const x = []
+	for (let i = 0; i < 100; i++) {
+		x[i] = Math.sin(i / 10) + (Math.random() - 0.5) / 100
+	}
+
+	model.fit(x)
+	const future = model.predict(x, 30)
+	expect(future).toHaveLength(30)
+	for (let i = 0; i < 30; i++) {
+		expect(future[i]).toBeCloseTo(Math.sin(10 + i / 10), 0)
 	}
 })
