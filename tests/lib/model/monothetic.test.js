84101182a947a4d7d85ce285a15b21d456bd9fcf
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-02-08 21:50:46 +0900

    Add Monothetic Clustering (#38)

---
+++ tests/lib/model/monothetic.test.js
@@ -0,0 +1,22 @@
+import Matrix from '../../../lib/util/matrix.js'
+import MonotheticClustering from '../../../lib/model/monothetic.js'
+
+import { randIndex } from '../../../lib/evaluate/clustering.js'
+
+test('clustering', () => {
+	const model = new MonotheticClustering()
+	const n = 50
+	const x = Matrix.randn(n, 2, 0, 0.1).concat(Matrix.randn(n, 2, 5, 0.1)).toArray()
+
+	model.init(x)
+	model.fit()
+	const y = model.predict()
+	expect(y).toHaveLength(x.length)
+
+	const t = []
+	for (let i = 0; i < x.length; i++) {
+		t[i] = Math.floor(i / n)
+	}
+	const ri = randIndex(y, t)
+	expect(ri).toBeGreaterThan(0.9)
+})
