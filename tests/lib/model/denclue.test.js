608ad550c38a7abeea552a534e3943dd7f2f3c22
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-02-16 21:54:56 +0900

    Add DENCLUE (#53)

---
+++ tests/lib/model/denclue.test.js
@@ -0,0 +1,30 @@
+import { jest } from '@jest/globals'
+jest.retryTimes(3)
+
+import Matrix from '../../../lib/util/matrix.js'
+import DENCLUE from '../../../lib/model/denclue.js'
+
+import { randIndex } from '../../../lib/evaluate/clustering.js'
+
+test.each([1, 2])('clustering %s', version => {
+	const model = new DENCLUE(0.2, version)
+	const n = 50
+	const x = Matrix.randn(n, 2, 0, 0.1)
+		.concat(Matrix.randn(n, 2, 5, 0.1))
+		.concat(Matrix.randn(n, 2, [0, 5], 0.1))
+		.toArray()
+
+	model.init(x)
+	for (let i = 0; i < 100; i++) {
+		model.fit()
+	}
+	const y = model.predict()
+	expect(y).toHaveLength(x.length)
+
+	const t = []
+	for (let i = 0; i < x.length; i++) {
+		t[i] = Math.floor(i / n)
+	}
+	const ri = randIndex(y, t)
+	expect(ri).toBeGreaterThan(0.9)
+})
