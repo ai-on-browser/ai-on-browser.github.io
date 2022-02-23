85293cd14927ee767362b381ed038711468c9650
ishii-norimi <mirasunimoni@yahoo.co.jp>
2022-02-23 19:14:03 +0900

    Add Genetic k-means

---
+++ js/view/genetic_kmeans.js
@@ -0,0 +1,39 @@
+import GeneticKMeans from '../../lib/model/genetic_kmeans.js'
+
+var dispGKMeans = function (elm, platform) {
+	let model = null
+
+	elm.append('span').text('k')
+	elm.append('input').attr('name', 'k').attr('type', 'number').attr('min', 1).attr('max', 100).attr('value', 3)
+	platform.setting.ml.controller
+		.stepLoopButtons()
+		.init(() => {
+			platform.init()
+			const k = +elm.select('[name=k]').property('value')
+			model = new GeneticKMeans(k, 10)
+			platform.fit((tx, ty) => {
+				model.init(tx)
+			})
+		})
+		.step(cb => {
+			platform.fit((tx, ty, pred_cb) => {
+				model.fit()
+				const pred = model.predict(tx)
+				pred_cb(pred.map(v => v + 1))
+			})
+			platform.centroids(
+				model.centroids,
+				model.centroids.map((c, i) => i + 1),
+				{
+					line: true,
+					duration: 1000,
+				}
+			)
+			cb && setTimeout(cb, 1000)
+		})
+}
+
+export default function (platform) {
+	platform.setting.ml.usage = 'Click and add data point. Then, click "Step" button repeatedly.'
+	dispGKMeans(platform.setting.ml.configElement, platform)
+}
---
+++ tests/lib/model/genetic_kmeans.test.js
@@ -0,0 +1,24 @@
+import Matrix from '../../../lib/util/matrix.js'
+import GeneticKMeans from '../../../lib/model/genetic_kmeans.js'
+
+import { randIndex } from '../../../lib/evaluate/clustering.js'
+
+test('predict', () => {
+	const model = new GeneticKMeans(2, 10)
+	const n = 50
+	const x = Matrix.randn(n, 2, 0, 0.1).concat(Matrix.randn(n, 2, 5, 0.1)).toArray()
+
+	model.init(x)
+	for (let i = 0; i < 20; i++) {
+		model.fit()
+	}
+	const y = model.predict(x)
+	expect(y).toHaveLength(x.length)
+
+	const t = []
+	for (let i = 0; i < x.length; i++) {
+		t[i] = Math.floor(i / n)
+	}
+	const ri = randIndex(y, t)
+	expect(ri).toBeGreaterThan(0.9)
+})
