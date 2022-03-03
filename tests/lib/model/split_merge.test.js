bbd257b776d06ad62ef4a18d9e23798c7582def8
ishii-norimi <mirasunimoni@yahoo.co.jp>
2022-03-03 20:02:51 +0900

    Add test for split and merge

---
+++ tests/lib/model/split_merge.test.js
@@ -0,0 +1,25 @@
+import Matrix from '../../../lib/util/matrix.js'
+import SplitAndMerge from '../../../lib/model/split_merge.js'
+
+import { randIndex } from '../../../lib/evaluate/clustering.js'
+
+test('predict', () => {
+	const model = new SplitAndMerge()
+	const n = 100
+	const x = Matrix.zeros(n, n).toArray()
+	const t = []
+	for (let i = 0, p = 0; i < n; i++) {
+		for (let j = 0; j < n; j++, p++) {
+			if ((i - n / 2) ** 2 + (j - n / 2) ** 2 < (n / 4) ** 2) {
+				x[i][j] = 255
+				t[p] = 1
+			} else {
+				t[p] = 0
+			}
+		}
+	}
+
+	const y = model.predict(x)
+	const ri = randIndex(y.flat(), t)
+	expect(ri).toBeGreaterThan(0.9)
+})
