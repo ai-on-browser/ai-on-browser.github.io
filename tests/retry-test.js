d826422bd92f1670440da1008d6228d0e33d7e14
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-04-16 09:48:02 +0900

    Show slow test (#150)
    * Show slow test
    
    * Accelerate some tests

---
+++ tests/retry-test.js
@@ -0,0 +1,40 @@
+import path from 'path'
+import url from 'url'
+
+const filepath = path.dirname(url.fileURLToPath(import.meta.url))
+
+class JestRetryTestReporter {
+	constructor(globalConfig, options) {
+		this._globalConfig = globalConfig
+		this._options = options
+
+		this._retryTests = []
+	}
+
+	onRunComplete() {
+		if (this._retryTests.length === 0) {
+			return
+		}
+		this._retryTests.sort((a, b) => b.invocations - a.invocations)
+		console.log()
+		console.log('Retry tests')
+		for (const test of this._retryTests) {
+			console.log(`  ${test.fullName}`)
+			console.log(`    retry ${test.invocations - 1} time(s) ${test.filePath.slice(filepath.length + 1)} `)
+		}
+	}
+
+	onTestResult(test, testResult) {
+		for (const test of testResult.testResults) {
+			if (test.invocations > 1) {
+				this._retryTests.push({
+					invocations: test.invocations,
+					fullName: test.fullName,
+					filePath: testResult.testFilePath,
+				})
+			}
+		}
+	}
+}
+
+export default JestRetryTestReporter
---
+++ tests/slow-test.js
@@ -0,0 +1,47 @@
+import path from 'path'
+import url from 'url'
+
+const filepath = path.dirname(url.fileURLToPath(import.meta.url))
+
+class JestSlowTestReporter {
+	constructor(globalConfig, options) {
+		this._globalConfig = globalConfig
+		this._options = options
+		this._slowTests = []
+	}
+
+	onRunComplete() {
+		console.log()
+		this._slowTests.sort((a, b) => b.duration - a.duration)
+		const slowestTests = this._slowTests.slice(0, this._options.numTests || 10)
+		const slowTestTime = slowestTests.reduce((total, st) => total + st.duration, 0)
+		const allTestTime = this._slowTests.reduce((total, st) => total + st.duration, 0)
+		const percentTime = (slowTestTime / allTestTime) * 100
+
+		console.log(
+			`Top ${slowestTests.length} slowest tests (${slowTestTime / 1000} seconds,` +
+				` ${percentTime.toFixed(1)}% of total time):`
+		)
+
+		for (let i = 0; i < slowestTests.length; i++) {
+			const duration = slowestTests[i].duration
+			const fullName = slowestTests[i].fullName
+			const filePath = slowestTests[i].filePath.slice(filepath.length + 1)
+
+			console.log(`  ${fullName}`)
+			console.log(`    ${duration / 1000}s ${filePath}`)
+		}
+	}
+
+	onTestResult(test, testResult) {
+		for (const test of testResult.testResults) {
+			this._slowTests.push({
+				duration: test.duration,
+				fullName: test.fullName,
+				filePath: testResult.testFilePath,
+			})
+		}
+	}
+}
+
+export default JestSlowTestReporter
