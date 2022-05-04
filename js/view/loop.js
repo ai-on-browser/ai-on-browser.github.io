819436461cc4ad837dcaa6c80506baea7e49cc92
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-05-04 12:05:02 +0900

    Add LoOP (#185)

---
+++ js/view/loop.js
@@ -0,0 +1,16 @@
+import LoOP from '../../lib/model/loop.js'
+import Controller from '../controller.js'
+
+export default function (platform) {
+	platform.setting.ml.usage = 'Click and add data point. Then, click "Calculate".'
+	const controller = new Controller(platform)
+	const calc = () => {
+		const model = new LoOP(k.value)
+		const outliers = model.predict(platform.trainInput)
+		platform.trainResult = outliers.map(v => v > t.value)
+	}
+
+	const k = controller.input.number({ label: ' k = ', min: 1, max: 100, value: 5 }).on('change', calc)
+	const t = controller.input.number({ label: ' t = ', min: 0, max: 1, step: 0.1, value: 0.5 }).on('change', calc)
+	controller.input.button('Calculate').on('click', calc)
+}
