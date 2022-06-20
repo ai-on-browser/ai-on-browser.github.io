addcf22b6b7b815f45bb87f35a61f2fb0e5a6159
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-06-20 21:02:40 +0900

    Keep tree structure in DIANA clustering (#261)

---
+++ tests/gui/view/diana.test.js
@@ -0,0 +1,66 @@
+import puppeteer from 'puppeteer'
+
+/** @type {puppeteer.Browser} */
+let browser
+beforeAll(async () => {
+	browser = await puppeteer.launch({ args: ['--no-sandbox'] })
+})
+
+afterAll(async () => {
+	await browser.close()
+})
+
+describe('clustering', () => {
+	/** @type {puppeteer.Page} */
+	let page
+	beforeEach(async () => {
+		page = await browser.newPage()
+		await page.goto(`http://${process.env.SERVER_HOST}/`)
+		page.on('console', message => console.log(`${message.type().substring(0, 3).toUpperCase()} ${message.text()}`))
+			.on('pageerror', ({ message }) => console.log(message))
+			.on('requestfailed', request => console.log(`${request.failure().errorText} ${request.url()}`))
+		await page.waitForSelector('#data_menu > *')
+	}, 10000)
+
+	test('initialize', async () => {
+		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		taskSelectBox.select('CT')
+		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		modelSelectBox.select('diana')
+		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
+		const buttons = await methodMenu.waitForSelector('.buttons')
+
+		const clusters = await buttons.waitForSelector('span:last-child')
+		await expect(clusters.evaluate(el => el.textContent)).resolves.toBe('')
+	}, 10000)
+
+	test('learn', async () => {
+		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		taskSelectBox.select('CT')
+		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		modelSelectBox.select('diana')
+		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
+		const buttons = await methodMenu.waitForSelector('.buttons')
+
+		const initButton = await buttons.waitForSelector('input[value=Initialize]')
+		await initButton.evaluate(el => el.click())
+
+		const clusters = await buttons.waitForSelector('span:last-child')
+		await expect(clusters.evaluate(el => el.textContent)).resolves.toBe('0')
+
+		const stepButton = await buttons.waitForSelector('input[value=Step]')
+		await stepButton.evaluate(el => el.click())
+
+		await expect(clusters.evaluate(el => el.textContent)).resolves.toBe('2')
+
+		const svg = await page.waitForSelector('#plot-area svg')
+		await svg.waitForSelector('.datas circle')
+		const circles = await svg.$$('.datas circle')
+		const colors = new Set()
+		for (const circle of circles) {
+			const fill = await circle.evaluate(el => el.getAttribute('fill'))
+			colors.add(fill)
+		}
+		expect(colors.size).toBe(2)
+	}, 10000)
+})
