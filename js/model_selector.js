bbf0d12d5da52e402a505e5f97f60d7c9e4cfdd7
ishii-norimi <mirasunimoni@yahoo.co.jp>
2022-07-02 19:52:40 +0900

    Add CHAMELEON

--- js/model_selector.js
+++ js/model_selector.js
@@ -110,6 +110,7 @@ const AIMethods = [
 				{ value: 'svc', title: 'Support vector clustering' },
 				{ value: 'affinity_propagation', title: 'Affinity Propagation' },
 				{ value: 'cast', title: 'CAST' },
+				{ value: 'chameleon', title: 'CHAMELEON' },
 				{ value: 'plsa', title: 'PLSA' },
 				{ value: 'latent_dirichlet_allocation', title: 'Latent Dirichlet Allocation' },
 				{ value: 'nmf', title: 'NMF' },
