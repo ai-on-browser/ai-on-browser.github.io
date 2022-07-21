270b0fd1dc315f0340e36433efb0902860d46d05
ishii-norimi <mirasunimoni@yahoo.co.jp>
2022-07-21 21:10:35 +0900

    Add multiclass perceptron

--- js/view/perceptron.js
+++ js/view/perceptron.js
@@ -1,18 +1,20 @@
-import Perceptron from '../../lib/model/perceptron.js'
+import { Perceptron, MulticlassPerceptron } from '../../lib/model/perceptron.js'
 import EnsembleBinaryModel from '../../lib/model/ensemble_binary.js'
 import Controller from '../controller.js'
 
-var dispPerceptron = function (elm, platform) {
+export default function (platform) {
+	platform.setting.ml.usage = 'Click and add data point. Then, click "Step".'
 	const controller = new Controller(platform)
 	let model = null
 	const calc = cb => {
-		const method = elm.select('[name=method]').property('value')
-		const average = elm.select('[name=average]').property('value')
-		const rate = +elm.select('[name=rate]').property('value')
 		if (!model) {
-			model = new EnsembleBinaryModel(function () {
-				return new Perceptron(average === 'average', rate)
-			}, method)
+			if (type.value === 'multiclass') {
+				model = new MulticlassPerceptron(rate.value)
+			} else {
+				model = new EnsembleBinaryModel(function () {
+					return new Perceptron(type.value === 'average', rate.value)
+				}, method.value)
+			}
 			model.init(
 				platform.trainInput,
 				platform.trainOutput.map(v => v[0])
@@ -25,30 +27,15 @@ var dispPerceptron = function (elm, platform) {
 		cb && cb()
 	}
 
-	elm.append('select')
-		.attr('name', 'average')
-		.selectAll('option')
-		.data(['', 'average'])
-		.enter()
-		.append('option')
-		.property('value', d => d)
-		.text(d => d)
-	elm.append('select')
-		.attr('name', 'method')
-		.selectAll('option')
-		.data(['oneone', 'onerest'])
-		.enter()
-		.append('option')
-		.property('value', d => d)
-		.text(d => d)
-	elm.append('span').text(' Learning rate ')
-	elm.append('input')
-		.attr('type', 'number')
-		.attr('name', 'rate')
-		.attr('min', 0)
-		.attr('max', 100)
-		.attr('step', 0.1)
-		.attr('value', 0.1)
+	const type = controller.select(['', 'average', 'multiclass']).on('change', () => {
+		if (type.value === 'multiclass') {
+			method.element.style.display = 'none'
+		} else {
+			method.element.style.display = null
+		}
+	})
+	const method = controller.select(['oneone', 'onerest'])
+	const rate = controller.input.number({ label: ' Learning rate ', min: 0, max: 100, step: 0.1, value: 0.1 })
 	controller
 		.stepLoopButtons()
 		.init(() => {
@@ -57,9 +44,4 @@ var dispPerceptron = function (elm, platform) {
 		})
 		.step(calc)
 		.epoch()
-}
-
-export default function (platform) {
-	platform.setting.ml.usage = 'Click and add data point. Then, click "Step".'
-	dispPerceptron(platform.setting.ml.configElement, platform)
 }
