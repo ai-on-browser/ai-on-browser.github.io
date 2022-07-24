5453ca5cd7fff7e867ec2f857f3dbbb45eb5a8fa
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-07-24 13:24:06 +0900

    Change return value of NNBCA (#290)

--- tests/lib/model/nnbca.test.js
+++ tests/lib/model/nnbca.test.js
@@ -17,3 +17,17 @@ test.each([undefined, 'euclid', 'manhattan', 'chebyshev', 'minkowski'])('predict
 	const acc = accuracy(y, t)
 	expect(acc).toBeGreaterThan(0.9)
 })
+
+test('predict null', () => {
+	const model = new NNBCA()
+	const x = Matrix.concat(Matrix.randn(50, 2, 0, 0.2), Matrix.randn(50, 2, 5, 0.2)).toArray()
+	const t = []
+	for (let i = 0; i < x.length; i++) {
+		t[i] = String.fromCharCode('a'.charCodeAt(0) + Math.floor(i / 50))
+	}
+
+	model.fit(x, t)
+	const y = model.predict([[-100, -100]])
+	expect(y).toHaveLength(1)
+	expect(y[0]).toBeNull()
+})
