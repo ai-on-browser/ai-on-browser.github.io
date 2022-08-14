56c2789fd8705003df9b8622838664439885ae1f
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-08-14 15:08:31 +0900

    Add ADAMENN (#308)
    * Add ADAMENN
    
    * Remove unused property

---
+++ tests/gui/view/adamenn.test.js
@@ -0,0 +1,54 @@
+import puppeteer from 'puppeteer'
+
+import { getPage } from '../helper/browser'
+
+describe('classification', () => {
+	/** @type {puppeteer.Page} */
+	let page
+	beforeEach(async () => {
+		page = await getPage()
+	}, 10000)
+
+	afterEach(async () => {
+		await page?.close()
+	})
+
+	test('initialize', async () => {
+		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		taskSelectBox.select('CF')
+		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		modelSelectBox.select('adamenn')
+		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
+		const buttons = await methodMenu.waitForSelector('.buttons')
+
+		const k0 = await buttons.waitForSelector('input:nth-of-type(1)')
+		await expect((await k0.getProperty('value')).jsonValue()).resolves.toBe('30')
+		const k1 = await buttons.waitForSelector('input:nth-of-type(2)')
+		await expect((await k1.getProperty('value')).jsonValue()).resolves.toBe('3')
+		const k2 = await buttons.waitForSelector('input:nth-of-type(3)')
+		await expect((await k2.getProperty('value')).jsonValue()).resolves.toBe('45')
+		const l = await buttons.waitForSelector('input:nth-of-type(4)')
+		await expect((await l.getProperty('value')).jsonValue()).resolves.toBe('23')
+		const k = await buttons.waitForSelector('input:nth-of-type(5)')
+		await expect((await k.getProperty('value')).jsonValue()).resolves.toBe('3')
+		const c = await buttons.waitForSelector('input:nth-of-type(6)')
+		await expect((await c.getProperty('value')).jsonValue()).resolves.toBe('0.5')
+	}, 10000)
+
+	test('learn', async () => {
+		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		taskSelectBox.select('CF')
+		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		modelSelectBox.select('adamenn')
+		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
+		const buttons = await methodMenu.waitForSelector('.buttons')
+
+		const methodFooter = await page.waitForSelector('#method_footer')
+		await expect(methodFooter.evaluate(el => el.textContent)).resolves.toBe('')
+
+		const calculateButton = await buttons.waitForSelector('input[value=Calculate]')
+		await calculateButton.evaluate(el => el.click())
+
+		await expect(methodFooter.evaluate(el => el.textContent)).resolves.toMatch(/^Accuracy:[0-9.]+$/)
+	}, 100000)
+})
