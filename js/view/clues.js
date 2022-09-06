70fadd17775d40ce17cb8a4b2afcc46b6e02939d
ishii-norimi <mirasunimoni@yahoo.co.jp>
2022-09-06 22:56:17 +0900

    Add CLUES

---
+++ js/view/clues.js
@@ -0,0 +1,19 @@
+import CLUES from '../../lib/model/clues.js'
+import Controller from '../controller.js'
+
+export default function (platform) {
+	platform.setting.ml.usage = 'Click and add data point. Then, click "Fit" button.'
+	const controller = new Controller(platform)
+
+	const fitModel = () => {
+		const model = new CLUES(alpha.value)
+		model.fit(platform.trainInput)
+		const pred = model.predict()
+		platform.trainResult = pred.map(v => v + 1)
+		clusters.value = model.size
+	}
+
+	const alpha = controller.input.number({ label: 'alpha', min: 0.01, max: 10, step: 0.01, value: 0.05 })
+	controller.input.button('Fit').on('click', fitModel)
+	const clusters = controller.text({ label: ' Clusters: ' })
+}
---
+++ tests/lib/model/clues.test.js
@@ -0,0 +1,24 @@
+// import { jest } from '@jest/globals'
+// jest.retryTimes(10)
+
+import Matrix from '../../../lib/util/matrix.js'
+import CLUES from '../../../lib/model/clues.js'
+
+import { randIndex } from '../../../lib/evaluate/clustering.js'
+
+test('clustering', () => {
+	const model = new CLUES(0.1)
+	const n = 100
+	const x = Matrix.concat(Matrix.randn(n, 2, 0, 0.1), Matrix.randn(n, 2, 5, 0.1)).toArray()
+
+	model.fit(x)
+	const y = model.predict()
+	expect(y).toHaveLength(x.length)
+
+	const t = []
+	for (let i = 0; i < x.length; i++) {
+		t[i] = Math.floor(i / n)
+	}
+	const ri = randIndex(y, t)
+	expect(ri).toBeGreaterThan(0.9)
+})
