60c3d179c02a4b6aae4ad64d09e8c1e10b273f04
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-09-12 23:20:18 +0900

    Add Winnow (#337)

---
+++ tests/lib/model/winnow.test.js
@@ -0,0 +1,27 @@
+import Winnow from '../../../lib/model/winnow.js'
+
+import { accuracy } from '../../../lib/evaluate/classification.js'
+
+test.each([1, 2])('fit %i', version => {
+	const model = new Winnow(1.1, null, version)
+	const x = []
+	const n = 50
+	for (let i = 0; i < n * 2; i++) {
+		const v = Array(10).fill(0)
+		for (let j = 0; j < 5; j++) {
+			v[j + Math.floor(i / n) * 5] = Math.random() < 0.9 ? 1 : 0
+		}
+		x.push(v)
+	}
+	const t = []
+	for (let i = 0; i < x.length; i++) {
+		t[i] = Math.floor(i / 50) * 2 - 1
+	}
+	model.init(x, t)
+	for (let i = 0; i < 10; i++) {
+		model.fit()
+	}
+	const y = model.predict(x)
+	const acc = accuracy(y, t)
+	expect(acc).toBeGreaterThan(0.95)
+})
