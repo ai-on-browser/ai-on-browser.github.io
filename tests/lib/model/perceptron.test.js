705cda7f3a34c901d855e7ab01d6b71e587f5c24
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-09-18 16:03:52 +0900

    Separate averaged perceptron from perceptron (#351)

--- tests/lib/model/perceptron.test.js
+++ tests/lib/model/perceptron.test.js
@@ -1,30 +1,41 @@
 import Matrix from '../../../lib/util/matrix.js'
-import { Perceptron, MulticlassPerceptron } from '../../../lib/model/perceptron.js'
+import { Perceptron, AveragedPerceptron, MulticlassPerceptron } from '../../../lib/model/perceptron.js'
 
 import { accuracy } from '../../../lib/evaluate/classification.js'
 
 describe('perceptron', () => {
-	test('default', () => {
-		const model = new Perceptron(false, 0.1)
-		expect(model._r).toBe(0.1)
+	test('fit', () => {
+		const model = new Perceptron(0.1)
+		const x = Matrix.concat(Matrix.randn(50, 2, 0, 0.2), Matrix.randn(50, 2, 5, 0.2)).toArray()
+		const t = []
+		for (let i = 0; i < x.length; i++) {
+			t[i] = Math.floor(i / 50) * 2 - 1
+		}
+		model.init(x, t)
+		for (let i = 0; i < 1000; i++) {
+			model.fit()
+		}
+		const y = model.predict(x)
+		const acc = accuracy(y, t)
+		expect(acc).toBeGreaterThan(0.95)
 	})
+})
 
-	describe.each([undefined, false, true])('average %p', average => {
-		test('fit', () => {
-			const model = new Perceptron(average, 0.1)
-			const x = Matrix.concat(Matrix.randn(50, 2, 0, 0.2), Matrix.randn(50, 2, 5, 0.2)).toArray()
-			const t = []
-			for (let i = 0; i < x.length; i++) {
-				t[i] = Math.floor(i / 50) * 2 - 1
-			}
-			model.init(x, t)
-			for (let i = 0; i < 1000; i++) {
-				model.fit()
-			}
-			const y = model.predict(x)
-			const acc = accuracy(y, t)
-			expect(acc).toBeGreaterThan(0.95)
-		})
+describe('averaged perceptron', () => {
+	test('fit', () => {
+		const model = new AveragedPerceptron(0.1)
+		const x = Matrix.concat(Matrix.randn(50, 2, 0, 0.2), Matrix.randn(50, 2, 5, 0.2)).toArray()
+		const t = []
+		for (let i = 0; i < x.length; i++) {
+			t[i] = Math.floor(i / 50) * 2 - 1
+		}
+		model.init(x, t)
+		for (let i = 0; i < 1000; i++) {
+			model.fit()
+		}
+		const y = model.predict(x)
+		const acc = accuracy(y, t)
+		expect(acc).toBeGreaterThan(0.95)
 	})
 })
 
