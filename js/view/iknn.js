15362152a16516ecb732a62011f83303f50a205b
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-10-21 22:39:47 +0900

    Add IKNN (#394)

---
+++ js/view/iknn.js
@@ -0,0 +1,21 @@
+import IKNN from '../../lib/model/iknn.js'
+import Controller from '../controller.js'
+
+export default function (platform) {
+	platform.setting.ml.usage = 'Click and add data point. Then, click "Calculate".'
+	const controller = new Controller(platform)
+	const fitModel = () => {
+		const model = new IKNN(k.value, i.value)
+
+		model.fit(
+			platform.trainInput,
+			platform.trainOutput.map(v => v[0])
+		)
+		const categories = model.predict(platform.testInput(4))
+		platform.testResult(categories)
+	}
+
+	const k = controller.input.number({ label: 'k', min: 1, max: 1000, value: 100 })
+	const i = controller.input.number({ label: 'i', min: 1, max: 1000, value: 5 })
+	controller.input.button('Calculate').on('click', fitModel)
+}
---
+++ tests/gui/view/iknn.test.js
@@ -0,0 +1,46 @@
+import puppeteer from 'puppeteer'
+
+import { getPage } from '../helper/browser'
+
+describe('classification', () => {
+	/** @type {puppeteer.Page} */
+	let page
+	beforeEach(async () => {
+		page = await getPage()
+	}, 10000)
+
+	afterEach(async () => {
+		await page?.close()
+	})
+
+	test('initialize', async () => {
+		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		taskSelectBox.select('CF')
+		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		modelSelectBox.select('iknn')
+		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
+		const buttons = await methodMenu.waitForSelector('.buttons')
+
+		const k = await buttons.waitForSelector('input:nth-of-type(1)')
+		await expect((await k.getProperty('value')).jsonValue()).resolves.toBe('100')
+		const i = await buttons.waitForSelector('input:nth-of-type(2)')
+		await expect((await i.getProperty('value')).jsonValue()).resolves.toBe('5')
+	}, 10000)
+
+	test('learn', async () => {
+		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		taskSelectBox.select('CF')
+		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		modelSelectBox.select('iknn')
+		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
+		const buttons = await methodMenu.waitForSelector('.buttons')
+
+		const methodFooter = await page.waitForSelector('#method_footer')
+		await expect(methodFooter.evaluate(el => el.textContent)).resolves.toBe('')
+
+		const calculateButton = await buttons.waitForSelector('input[value=Calculate]')
+		await calculateButton.evaluate(el => el.click())
+
+		await expect(methodFooter.evaluate(el => el.textContent)).resolves.toMatch(/^Accuracy:[0-9.]+$/)
+	}, 100000)
+})
