be10848e95359f8a5c7d90a51d5fe2d1be86830b
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-10-22 12:58:59 +0900

    Add Weighted KNN (#397)

---
+++ js/view/weighted_knn.js
@@ -0,0 +1,31 @@
+import WeightedKNN from '../../lib/model/weighted_knn.js'
+import Controller from '../controller.js'
+
+export default function (platform) {
+	platform.setting.ml.usage = 'Click and add data point. Then, click "Calculate".'
+	const controller = new Controller(platform)
+	const fitModel = () => {
+		const model = new WeightedKNN(k.value, metric.value, weight.value)
+
+		model.fit(
+			platform.trainInput,
+			platform.trainOutput.map(v => v[0])
+		)
+		const categories = model.predict(platform.testInput(4))
+		platform.testResult(categories)
+	}
+
+	const metric = controller.select(['euclid', 'manhattan', 'chebyshev'])
+	const weight = controller.select([
+		'gaussian',
+		'rectangular',
+		'triangular',
+		'epanechnikov',
+		'quartic',
+		'triweight',
+		'cosine',
+		'inversion',
+	])
+	const k = controller.input.number({ label: 'k', min: 1, max: 1000, value: 5 })
+	controller.input.button('Calculate').on('click', fitModel)
+}
---
+++ tests/lib/model/weighted_knn.test.js
@@ -0,0 +1,31 @@
+import Matrix from '../../../lib/util/matrix.js'
+import WeightedKNN from '../../../lib/model/weighted_knn.js'
+
+import { accuracy } from '../../../lib/evaluate/classification.js'
+
+describe.each([undefined, 'euclid', 'manhattan', 'chebyshev', 'minkowski'])('metric %s', metric => {
+	test.each([
+		undefined,
+		'gaussian',
+		'rectangular',
+		'triangular',
+		'epanechnikov',
+		'quartic',
+		'triweight',
+		'cosine',
+		'inversion',
+	])('predict %s', weight => {
+		const model = new WeightedKNN(10, metric, weight)
+		const x = Matrix.concat(Matrix.randn(50, 2, 0, 2), Matrix.randn(50, 2, 5, 2)).toArray()
+		const t = []
+		for (let i = 0; i < x.length; i++) {
+			t[i] = String.fromCharCode('a'.charCodeAt(0) + Math.floor(i / 50))
+		}
+
+		model.fit(x, t)
+		const y = model.predict(x)
+		expect(y).toHaveLength(x.length)
+		const acc = accuracy(y, t)
+		expect(acc).toBeGreaterThan(0.9)
+	})
+})
