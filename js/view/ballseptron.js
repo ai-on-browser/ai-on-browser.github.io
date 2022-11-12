6e91a8f5637c5a240d499748b8d3be6be5291279
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-11-12 23:26:12 +0900

    Add Ballseptron (#414)

---
+++ js/view/ballseptron.js
@@ -0,0 +1,36 @@
+import Ballseptron from '../../lib/model/ballseptron.js'
+import EnsembleBinaryModel from '../../lib/model/ensemble_binary.js'
+import Controller from '../controller.js'
+
+export default function (platform) {
+	platform.setting.ml.usage = 'Click and add data point. Then, click "Step".'
+	const controller = new Controller(platform)
+	let model = null
+	const calc = cb => {
+		if (!model) {
+			model = new EnsembleBinaryModel(function () {
+				return new Ballseptron(radius.value)
+			}, method.value)
+			model.init(
+				platform.trainInput,
+				platform.trainOutput.map(v => v[0])
+			)
+		}
+		model.fit()
+
+		const categories = model.predict(platform.testInput(3))
+		platform.testResult(categories)
+		cb && cb()
+	}
+
+	const method = controller.select(['oneone', 'onerest'])
+	const radius = controller.input.number({ label: ' radius ', min: 0, max: 100, step: 0.1, value: 0.1 })
+	controller
+		.stepLoopButtons()
+		.init(() => {
+			model = null
+			platform.init()
+		})
+		.step(calc)
+		.epoch()
+}
