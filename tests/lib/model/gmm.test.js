043705a531d25547a1df5512d69948032b6d796c
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-11-16 18:15:32 +0900

    Improve some tests (#424)
    * Improve some tests
    
    * Increase retry time

--- tests/lib/model/gmm.test.js
+++ tests/lib/model/gmm.test.js
@@ -1,5 +1,5 @@
 import { jest } from '@jest/globals'
-jest.retryTimes(5)
+jest.retryTimes(10)
 
 import Matrix from '../../../lib/util/matrix.js'
 import { GMM, GMR, SemiSupervisedGMM } from '../../../lib/model/gmm.js'
--- tests/lib/model/lof.test.js
+++ tests/lib/model/lof.test.js
@@ -1,3 +1,6 @@
+import { jest } from '@jest/globals'
+jest.retryTimes(3)
+
 import Matrix from '../../../lib/util/matrix.js'
 import LOF from '../../../lib/model/lof.js'
 
