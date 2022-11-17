5996e29ea9f60831f4103db3a6c4045da52d7e33
ishii-norimi <mirasunimoni@yahoo.co.jp>
2022-11-17 18:02:33 +0900

    Add evaluation indicator

---
+++ tests/lib/evaluate/classification.test.js
@@ -0,0 +1,270 @@
+import { accuracy, precision, recall, fScore, cohensKappa } from '../../../lib/evaluate/classification.js'
+
+describe('accuracy', () => {
+	test('definition two classes', () => {
+		const a = []
+		const b = []
+		const n = 1000
+		let same = 0
+		for (let i = 0; i < n; i++) {
+			const ai = Math.floor(Math.random() * 2)
+			const bi = Math.floor(Math.random() * 2)
+			a.push(ai)
+			b.push(bi)
+			if (ai === bi) {
+				same++
+			}
+		}
+		const acc = accuracy(a, b)
+		expect(acc).toBe(same / n)
+	})
+
+	test('best', () => {
+		const a = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			a.push(Math.floor(Math.random() * 3))
+		}
+		const acc = accuracy(a, a)
+		expect(acc).toBe(1)
+	})
+
+	test('worst', () => {
+		const a = []
+		const b = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			const v = Math.floor(Math.random() * 3)
+			a.push(v)
+			b.push((v + 1) % 3)
+		}
+		const acc = accuracy(a, b)
+		expect(acc).toBe(0)
+	})
+})
+
+describe('precision', () => {
+	test('definition two classes', () => {
+		const a = []
+		const b = []
+		const n = 1000
+		let tp1 = 0
+		let fp1 = 0
+		let tp2 = 0
+		let fp2 = 0
+		for (let i = 0; i < n; i++) {
+			const ai = Math.floor(Math.random() * 2)
+			const bi = Math.floor(Math.random() * 2)
+			a.push(ai)
+			b.push(bi)
+			if (ai === bi) {
+				if (ai === 0) {
+					tp1++
+				} else {
+					tp2++
+				}
+			} else {
+				if (ai === 0) {
+					fp1++
+				} else {
+					fp2++
+				}
+			}
+		}
+		const pre = precision(a, b)
+		expect(pre).toBe((tp1 / (tp1 + fp1) + tp2 / (tp2 + fp2)) / 2)
+	})
+
+	test('best', () => {
+		const a = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			a.push(Math.floor(Math.random() * 3))
+		}
+		const acc = precision(a, a)
+		expect(acc).toBe(1)
+	})
+
+	test('worst', () => {
+		const a = []
+		const b = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			const v = Math.floor(Math.random() * 3)
+			a.push(v)
+			b.push((v + 1) % 3)
+		}
+		const acc = precision(a, b)
+		expect(acc).toBe(0)
+	})
+})
+
+describe('recall', () => {
+	test('definition two classes', () => {
+		const a = []
+		const b = []
+		const n = 1000
+		let tp1 = 0
+		let fn1 = 0
+		let tp2 = 0
+		let fn2 = 0
+		for (let i = 0; i < n; i++) {
+			const ai = Math.floor(Math.random() * 2)
+			const bi = Math.floor(Math.random() * 2)
+			a.push(ai)
+			b.push(bi)
+			if (ai === bi) {
+				if (ai === 0) {
+					tp1++
+				} else {
+					tp2++
+				}
+			} else {
+				if (ai === 0) {
+					fn2++
+				} else {
+					fn1++
+				}
+			}
+		}
+		const rec = recall(a, b)
+		expect(rec).toBe((tp1 / (tp1 + fn1) + tp2 / (tp2 + fn2)) / 2)
+	})
+
+	test('best', () => {
+		const a = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			a.push(Math.floor(Math.random() * 3))
+		}
+		const acc = recall(a, a)
+		expect(acc).toBe(1)
+	})
+
+	test('worst', () => {
+		const a = []
+		const b = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			const v = Math.floor(Math.random() * 3)
+			a.push(v)
+			b.push((v + 1) % 3)
+		}
+		const acc = recall(a, b)
+		expect(acc).toBe(0)
+	})
+})
+
+describe('fScore', () => {
+	test('definition two classes', () => {
+		const a = []
+		const b = []
+		const n = 1000
+		let tp1 = 0
+		let fp1 = 0
+		let fn1 = 0
+		let tp2 = 0
+		let fp2 = 0
+		let fn2 = 0
+		for (let i = 0; i < n; i++) {
+			const ai = Math.floor(Math.random() * 2)
+			const bi = Math.floor(Math.random() * 2)
+			a.push(ai)
+			b.push(bi)
+			if (ai === bi) {
+				if (ai === 0) {
+					tp1++
+				} else {
+					tp2++
+				}
+			} else {
+				if (ai === 0) {
+					fn2++
+					fp1++
+				} else {
+					fn1++
+					fp2++
+				}
+			}
+		}
+		const fs = fScore(a, b)
+		expect(fs).toBe(((2 * tp1) / (2 * tp1 + fp1 + fn1) + (2 * tp2) / (2 * tp2 + fp2 + fn2)) / 2)
+	})
+
+	test('best', () => {
+		const a = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			a.push(Math.floor(Math.random() * 3))
+		}
+		const acc = fScore(a, a)
+		expect(acc).toBe(1)
+	})
+
+	test('worst', () => {
+		const a = []
+		const b = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			const v = Math.floor(Math.random() * 3)
+			a.push(v)
+			b.push((v + 1) % 3)
+		}
+		const acc = fScore(a, b)
+		expect(acc).toBe(0)
+	})
+})
+
+describe('cohensKappa', () => {
+	test('definition two classes', () => {
+		const a = []
+		const b = []
+		const n = 1000
+		let c = 0
+		for (let i = 0; i < n; i++) {
+			const ai = Math.floor(Math.random() * 2)
+			const bi = Math.floor(Math.random() * 2)
+			a.push(ai)
+			b.push(bi)
+			if (ai === bi) {
+				c++
+			}
+		}
+		const p0 = c / n
+		const pe =
+			(a.reduce((s, v) => s + (v === 0 ? 1 : 0), 0) * b.reduce((s, v) => s + (v === 0 ? 1 : 0), 0) +
+				a.reduce((s, v) => s + (v === 1 ? 1 : 0), 0) * b.reduce((s, v) => s + (v === 1 ? 1 : 0), 0)) /
+			n ** 2
+		const ck = cohensKappa(a, b)
+		expect(ck).toBe((p0 - pe) / (1 - pe))
+	})
+
+	test('best', () => {
+		const a = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			a.push(Math.floor(Math.random() * 3))
+		}
+		const acc = cohensKappa(a, a)
+		expect(acc).toBe(1)
+	})
+
+	test('worst', () => {
+		const a = []
+		const b = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			const v = Math.floor(Math.random() * 3)
+			a.push(v)
+			b.push((v + 1) % 3)
+		}
+		const acc = cohensKappa(a, b)
+
+		const pe =
+			(a.reduce((s, v) => s + (v === 0 ? 1 : 0), 0) * b.reduce((s, v) => s + (v === 0 ? 1 : 0), 0) +
+				a.reduce((s, v) => s + (v === 1 ? 1 : 0), 0) * b.reduce((s, v) => s + (v === 1 ? 1 : 0), 0) +
+				a.reduce((s, v) => s + (v === 2 ? 1 : 0), 0) * b.reduce((s, v) => s + (v === 2 ? 1 : 0), 0)) /
+			n ** 2
+		expect(acc).toBe(-pe / (1 - pe))
+	})
+})
---
+++ tests/lib/evaluate/clustering.test.js
@@ -0,0 +1,414 @@
+import {
+	davisBouldinIndex,
+	dunnIndex,
+	silhouetteCoefficient,
+	purity,
+	randIndex,
+	fScore,
+	jaccardIndex,
+	fowlkesMallowsIndex,
+} from '../../../lib/evaluate/clustering.js'
+
+describe('davisBouldinIndex', () => {
+	test('definition', () => {
+		const data = []
+		const y = []
+		const n = 1000
+		const dim = 3
+		const count = [0, 0]
+		const center = [Array(dim).fill(0), Array(dim).fill(0)]
+		for (let i = 0; i < n; i++) {
+			const ai = Math.floor(Math.random() * 2)
+			y.push(ai)
+			count[ai]++
+			data[i] = []
+			for (let d = 0; d < dim; d++) {
+				data[i][d] = Math.random()
+				center[ai][d] += data[i][d]
+			}
+		}
+		const s = [0, 0]
+		for (let k = 0; k < 2; k++) {
+			center[k] = center[k].map(v => v / count[k])
+			for (let i = 0; i < n; i++) {
+				if (y[i] !== k) {
+					continue
+				}
+				s[k] += Math.sqrt(data[i].reduce((s, v, d) => s + (v - center[k][d]) ** 2, 0))
+			}
+			s[k] /= count[k]
+		}
+		const m01 = Math.sqrt(center[0].reduce((s, v, d) => s + (v - center[1][d]) ** 2, 0))
+
+		const dbi = davisBouldinIndex(data, y)
+		expect(dbi).toBe((s[0] + s[1]) / m01)
+	})
+})
+
+describe('dunnIndex', () => {
+	describe('definition', () => {
+		test('intra=max, inter=centroid', () => {
+			const data = []
+			const y = []
+			const n = 1000
+			const dim = 3
+			const count = [0, 0]
+			const center = [Array(dim).fill(0), Array(dim).fill(0)]
+			for (let i = 0; i < n; i++) {
+				const ai = Math.floor(Math.random() * 2)
+				y.push(ai)
+				count[ai]++
+				data[i] = []
+				for (let d = 0; d < dim; d++) {
+					data[i][d] = Math.random()
+					center[ai][d] += data[i][d]
+				}
+			}
+
+			const d = [0, 0]
+			for (let k = 0; k < 2; k++) {
+				center[k] = center[k].map(v => v / count[k])
+				for (let i = 0; i < n; i++) {
+					if (y[i] !== k) {
+						continue
+					}
+					for (let j = 0; j < n; j++) {
+						if (y[j] !== k) {
+							continue
+						}
+						const dij = Math.sqrt(data[i].reduce((s, v, d) => s + (v - data[j][d]) ** 2, 0))
+						if (d[k] < dij) {
+							d[k] = dij
+						}
+					}
+				}
+			}
+			const dij = Math.sqrt(center[0].reduce((s, v, d) => s + (v - center[1][d]) ** 2, 0))
+
+			const di = dunnIndex(data, y)
+			expect(di).toBe(dij / Math.max(d[0], d[1]))
+		})
+
+		test('intra=mean, inter=centroid', () => {
+			const data = []
+			const y = []
+			const n = 1000
+			const dim = 3
+			const count = [0, 0]
+			const center = [Array(dim).fill(0), Array(dim).fill(0)]
+			for (let i = 0; i < n; i++) {
+				const ai = Math.floor(Math.random() * 2)
+				y.push(ai)
+				count[ai]++
+				data[i] = []
+				for (let d = 0; d < dim; d++) {
+					data[i][d] = Math.random()
+					center[ai][d] += data[i][d]
+				}
+			}
+
+			const d = [0, 0]
+			for (let k = 0; k < 2; k++) {
+				center[k] = center[k].map(v => v / count[k])
+				for (let i = 0; i < n; i++) {
+					if (y[i] !== k) {
+						continue
+					}
+					for (let j = 0; j < n; j++) {
+						if (i === j || y[j] !== k) {
+							continue
+						}
+						d[k] += Math.sqrt(data[i].reduce((s, v, d) => s + (v - data[j][d]) ** 2, 0))
+					}
+				}
+				d[k] *= 2 / (count[k] * (count[k] - 1))
+			}
+			const dij = Math.sqrt(center[0].reduce((s, v, d) => s + (v - center[1][d]) ** 2, 0))
+
+			const di = dunnIndex(data, y, 'mean')
+			expect(di).toBeCloseTo(dij / Math.max(d[0], d[1]))
+		})
+
+		test('intra=centroid, inter=centroid', () => {
+			const data = []
+			const y = []
+			const n = 1000
+			const dim = 3
+			const count = [0, 0]
+			const center = [Array(dim).fill(0), Array(dim).fill(0)]
+			for (let i = 0; i < n; i++) {
+				const ai = Math.floor(Math.random() * 2)
+				y.push(ai)
+				count[ai]++
+				data[i] = []
+				for (let d = 0; d < dim; d++) {
+					data[i][d] = Math.random()
+					center[ai][d] += data[i][d]
+				}
+			}
+
+			const d = [0, 0]
+			for (let k = 0; k < 2; k++) {
+				center[k] = center[k].map(v => v / count[k])
+				for (let i = 0; i < n; i++) {
+					if (y[i] !== k) {
+						continue
+					}
+					d[k] += Math.sqrt(data[i].reduce((s, v, d) => s + (v - center[k][d]) ** 2, 0))
+				}
+				d[k] /= count[k]
+			}
+			const dij = Math.sqrt(center[0].reduce((s, v, d) => s + (v - center[1][d]) ** 2, 0))
+
+			const di = dunnIndex(data, y, 'centroid')
+			expect(di).toBe(dij / Math.max(d[0], d[1]))
+		})
+	})
+})
+
+describe('silhouetteCoefficient', () => {
+	test('definition', () => {
+		const data = []
+		const y = []
+		const n = 1000
+		const dim = 3
+		const d = []
+		for (let i = 0; i < n; i++) {
+			const ai = Math.floor(Math.random() * 2)
+			y.push(ai)
+			data[i] = []
+			for (let d = 0; d < dim; d++) {
+				data[i][d] = Math.random()
+			}
+
+			d[i] = []
+			for (let j = 0; j < i; j++) {
+				d[i][j] = d[j][i] = Math.sqrt(data[i].reduce((s, v, m) => s + (v - data[j][m]) ** 2, 0))
+			}
+		}
+
+		const sc = silhouetteCoefficient(data, y)
+		expect(sc).toHaveLength(data.length)
+		for (let i = 0; i < n; i++) {
+			let a = 0
+			let ca = 0
+			for (let j = 0; j < n; j++) {
+				if (i === j || y[i] !== y[j]) {
+					continue
+				}
+				a += d[i][j]
+				ca++
+			}
+			a /= ca
+
+			let b = 0
+			let cb = 0
+			for (let j = 0; j < n; j++) {
+				if (y[i] === y[j]) {
+					continue
+				}
+				b += d[i][j]
+				cb++
+			}
+			b /= cb
+			const s = (b - a) / Math.max(a, b)
+
+			expect(sc[i]).toBe(s)
+		}
+	})
+})
+
+describe('purity', () => {
+	test('definition', () => {
+		const a = []
+		const b = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			const ai = Math.floor(Math.random() * 2)
+			const bi = Math.floor(Math.random() * 2)
+			a.push(ai)
+			b.push(bi)
+		}
+		let p = 0
+		for (let k = 0; k < 2; k++) {
+			let c = [0, 0]
+			for (let i = 0; i < n; i++) {
+				if (a[i] !== k) {
+					continue
+				}
+				c[b[i]]++
+			}
+			p += Math.max(c[0], c[1])
+		}
+		const pu = purity(a, b)
+		expect(pu).toBe(p / n)
+	})
+
+	test('best', () => {
+		const a = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			a.push(Math.floor(Math.random() * 3))
+		}
+		const pu = purity(a, a)
+		expect(pu).toBe(1)
+	})
+})
+
+describe('randIndex', () => {
+	test('definition', () => {
+		const a = []
+		const b = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			const ai = Math.floor(Math.random() * 2)
+			const bi = Math.floor(Math.random() * 2)
+			a.push(ai)
+			b.push(bi)
+		}
+		let s = 0
+		for (let i = 0; i < n; i++) {
+			for (let j = 0; j < i; j++) {
+				if ((a[i] === a[j] && b[i] === b[j]) || (a[i] !== a[j] && b[i] !== b[j])) {
+					s++
+				}
+			}
+		}
+		const ri = randIndex(a, b)
+		expect(ri).toBe(s / ((n * (n - 1)) / 2))
+	})
+
+	test('best', () => {
+		const a = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			a.push(Math.floor(Math.random() * 3))
+		}
+		const ri = randIndex(a, a)
+		expect(ri).toBe(1)
+	})
+})
+
+describe('fScore', () => {
+	describe('definition', () => {
+		test('default', () => {
+			const a = []
+			const b = []
+			const n = 1000
+			for (let i = 0; i < n; i++) {
+				const ai = Math.floor(Math.random() * 2)
+				const bi = Math.floor(Math.random() * 2)
+				a.push(ai)
+				b.push(bi)
+			}
+			let tp = 0
+			let fp = 0
+			let fn = 0
+			for (let i = 0; i < n; i++) {
+				for (let j = 0; j < i; j++) {
+					if (a[i] === a[j] && b[i] === b[j]) {
+						tp++
+					} else if (a[i] === a[j] && b[i] !== b[j]) {
+						fp++
+					} else if (a[i] !== a[j] && b[i] === b[j]) {
+						fn++
+					}
+				}
+			}
+			const p = tp / (tp + fp)
+			const r = tp / (tp + fn)
+			const fs = fScore(a, b)
+			expect(fs).toBeCloseTo((2 * (p * r)) / (p + r))
+		})
+	})
+
+	test('best', () => {
+		const a = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			a.push(Math.floor(Math.random() * 3))
+		}
+		const fs = fScore(a, a)
+		expect(fs).toBe(1)
+	})
+})
+
+describe('jaccardIndex', () => {
+	test('definition', () => {
+		const a = []
+		const b = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			const ai = Math.floor(Math.random() * 2)
+			const bi = Math.floor(Math.random() * 2)
+			a.push(ai)
+			b.push(bi)
+		}
+		let tp = 0
+		let fp = 0
+		let fn = 0
+		for (let i = 0; i < n; i++) {
+			for (let j = 0; j < i; j++) {
+				if (a[i] === a[j] && b[i] === b[j]) {
+					tp++
+				} else if (a[i] === a[j] && b[i] !== b[j]) {
+					fp++
+				} else if (a[i] !== a[j] && b[i] === b[j]) {
+					fn++
+				}
+			}
+		}
+		const ji = jaccardIndex(a, b)
+		expect(ji).toBe(tp / (tp + fp + fn))
+	})
+
+	test('best', () => {
+		const a = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			a.push(Math.floor(Math.random() * 3))
+		}
+		const ji = jaccardIndex(a, a)
+		expect(ji).toBe(1)
+	})
+})
+
+describe('fowlkesMallowsIndex', () => {
+	test('definition', () => {
+		const a = []
+		const b = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			const ai = Math.floor(Math.random() * 2)
+			const bi = Math.floor(Math.random() * 2)
+			a.push(ai)
+			b.push(bi)
+		}
+		let tp = 0
+		let fp = 0
+		let fn = 0
+		for (let i = 0; i < n; i++) {
+			for (let j = 0; j < i; j++) {
+				if (a[i] === a[j] && b[i] === b[j]) {
+					tp++
+				} else if (a[i] === a[j] && b[i] !== b[j]) {
+					fp++
+				} else if (a[i] !== a[j] && b[i] === b[j]) {
+					fn++
+				}
+			}
+		}
+		const fmi = fowlkesMallowsIndex(a, b)
+		expect(fmi).toBeCloseTo(Math.sqrt((tp / (tp + fp)) * (tp / (tp + fn))))
+	})
+
+	test('best', () => {
+		const a = []
+		const n = 1000
+		for (let i = 0; i < n; i++) {
+			a.push(Math.floor(Math.random() * 3))
+		}
+		const fmi = fowlkesMallowsIndex(a, a)
+		expect(fmi).toBe(1)
+	})
+})
