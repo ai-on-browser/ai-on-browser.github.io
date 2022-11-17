115f094bb8e2bd60f3b787b1ea5a53290544dfaf
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-11-17 22:43:09 +0900

    Add Stoptron (#429)

---
+++ js/view/stoptron.js
@@ -0,0 +1,37 @@
+import Stoptron from '../../lib/model/stoptron.js'
+import EnsembleBinaryModel from '../../lib/model/ensemble_binary.js'
+import Controller from '../controller.js'
+
+export default function (platform) {
+	platform.setting.ml.usage = 'Click and add data point. Then, click "Step".'
+	const controller = new Controller(platform)
+	let model = null
+	const calc = cb => {
+		if (!model) {
+			model = new EnsembleBinaryModel(function () {
+				return new Stoptron(cache.value, kernel.value)
+			}, method.value)
+			model.init(
+				platform.trainInput,
+				platform.trainOutput.map(v => v[0])
+			)
+		}
+		model.fit()
+
+		const categories = model.predict(platform.testInput(3))
+		platform.testResult(categories)
+		cb && cb()
+	}
+
+	const method = controller.select(['oneone', 'onerest'])
+	const kernel = controller.select(['gaussian', 'polynomial'])
+	const cache = controller.input.number({ label: ' cache size ', min: 0, max: 1000, value: 10 })
+	controller
+		.stepLoopButtons()
+		.init(() => {
+			model = null
+			platform.init()
+		})
+		.step(calc)
+		.epoch()
+}
