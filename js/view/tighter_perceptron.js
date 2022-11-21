8d38998b78206fd08051c71ecee308cc72264efa
ishii-norimi <mirasunimoni@yahoo.co.jp>
2022-11-21 22:58:05 +0900

    Add Tighter perceptron

---
+++ js/view/tighter_perceptron.js
@@ -0,0 +1,39 @@
+import TighterPerceptron from '../../lib/model/tighter_perceptron.js'
+import EnsembleBinaryModel from '../../lib/model/ensemble_binary.js'
+import Controller from '../controller.js'
+
+export default function (platform) {
+	platform.setting.ml.usage = 'Click and add data point. Then, click "Step".'
+	const controller = new Controller(platform)
+	let model = null
+	const calc = cb => {
+		if (!model) {
+			model = new EnsembleBinaryModel(function () {
+				return new TighterPerceptron(beta.value, p.value, update.value)
+			}, method.value)
+			model.init(
+				platform.trainInput,
+				platform.trainOutput.map(v => v[0])
+			)
+		}
+		model.fit()
+		console.log(model)
+
+		const categories = model.predict(platform.testInput(3))
+		platform.testResult(categories)
+		cb && cb()
+	}
+
+	const method = controller.select(['oneone', 'onerest'])
+	const beta = controller.input.number({ label: ' beta ', min: 0, max: 100, step: 0.1, value: 0.1 })
+	const p = controller.input.number({ label: ' p ', min: 0, max: 1000, value: 10 })
+	const update = controller.select(['perceptron', 'mira', 'nobias'])
+	controller
+		.stepLoopButtons()
+		.init(() => {
+			model = null
+			platform.init()
+		})
+		.step(calc)
+		.epoch()
+}
---
+++ tests/lib/model/tighter_perceptron.test.js
@@ -0,0 +1,26 @@
+import { jest } from '@jest/globals'
+jest.retryTimes(3)
+
+import Matrix from '../../../lib/util/matrix.js'
+import TighterPerceptron from '../../../lib/model/tighter_perceptron.js'
+
+import { accuracy } from '../../../lib/evaluate/classification.js'
+
+describe('classification', () => {
+	test.each([undefined, 'perceptron', 'mira', 'nobias'])('update %s', update => {
+		const model = new TighterPerceptron(1, 10, update)
+		const x = Matrix.concat(Matrix.randn(50, 2, 0, 0.2), Matrix.randn(50, 2, 5, 0.2)).toArray()
+		x[50] = [0.1, 0.1]
+		const t = []
+		for (let i = 0; i < x.length; i++) {
+			t[i] = Math.floor(i / 50) * 2 - 1
+		}
+		model.init(x, t)
+		for (let i = 0; i < 10; i++) {
+			model.fit()
+		}
+		const y = model.predict(x)
+		const acc = accuracy(y, t)
+		expect(acc).toBeGreaterThan(0.8)
+	})
+})
