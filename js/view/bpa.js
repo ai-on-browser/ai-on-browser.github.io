4a568b2a2162add32eb09a0137f6470d58e4deba
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-12-25 16:08:54 +0900

    Add BPA (#482)

---
+++ js/view/bpa.js
@@ -0,0 +1,43 @@
+import BPA from '../../lib/model/bpa.js'
+import EnsembleBinaryModel from '../../lib/model/ensemble_binary.js'
+import Controller from '../controller.js'
+
+export default function (platform) {
+	platform.setting.ml.usage = 'Click and add data point. Then, click "Step".'
+	platform.setting.ml.reference = {
+		author: 'Z. Wang, S. Vucetic',
+		title: 'Online Passive-Aggressive Algorithms on a Budget',
+		year: 2010,
+	}
+	const controller = new Controller(platform)
+	let model = null
+	const calc = cb => {
+		if (!model) {
+			model = new EnsembleBinaryModel(function () {
+				return new BPA(c.value, b.value, version.value, kernel.value)
+			}, method.value)
+		}
+		model.fit(
+			platform.trainInput,
+			platform.trainOutput.map(v => v[0])
+		)
+
+		const categories = model.predict(platform.testInput(3))
+		platform.testResult(categories)
+		cb && cb()
+	}
+
+	const method = controller.select(['oneone', 'onerest'])
+	const kernel = controller.select(['gaussian', 'polynomial'])
+	const version = controller.select(['simple', 'projecting', 'nn'])
+	const c = controller.input.number({ label: ' Regularization ', min: 0, max: 100, step: 0.1, value: 1 })
+	const b = controller.input.number({ label: ' Budget size ', min: 1, max: 1000, value: 10 })
+	controller
+		.stepLoopButtons()
+		.init(() => {
+			model = null
+			platform.init()
+		})
+		.step(calc)
+		.epoch()
+}
---
+++ tests/lib/model/bpa.test.js
@@ -0,0 +1,43 @@
+import { jest } from '@jest/globals'
+jest.retryTimes(5)
+
+import Matrix from '../../../lib/util/matrix.js'
+import BPA from '../../../lib/model/bpa.js'
+
+import { accuracy } from '../../../lib/evaluate/classification.js'
+
+describe('classification', () => {
+	describe.each([undefined, 'simple', 'projecting', 'nn'])('version %s', version => {
+		test.each([undefined, 'gaussian'])('kernel %s', kernel => {
+			const model = new BPA(1, 10, version, kernel)
+			const x = Matrix.concat(Matrix.randn(50, 2, 0, 0.2), Matrix.randn(50, 2, 5, 0.2)).toArray()
+			const t = []
+			for (let i = 0; i < x.length; i++) {
+				t[i] = Math.floor(i / 50) * 2 - 1
+			}
+			for (let i = 0; i < 10; i++) {
+				model.fit(x, t)
+			}
+			const y = model.predict(x)
+			const acc = accuracy(y, t)
+			expect(acc).toBeGreaterThan(0.8)
+		})
+
+		test('custom kernel', () => {
+			const model = new BPA(1, 10, version, (a, b) =>
+				Math.exp(-(a.reduce((s, v, i) => s + (v - b[i]) ** 2, 0) ** 2) / 0.01)
+			)
+			const x = Matrix.concat(Matrix.randn(50, 2, 0, 0.2), Matrix.randn(50, 2, 5, 0.2)).toArray()
+			const t = []
+			for (let i = 0; i < x.length; i++) {
+				t[i] = Math.floor(i / 50) * 2 - 1
+			}
+			for (let i = 0; i < 10; i++) {
+				model.fit(x, t)
+			}
+			const y = model.predict(x)
+			const acc = accuracy(y, t)
+			expect(acc).toBeGreaterThan(0.9)
+		})
+	})
+})
