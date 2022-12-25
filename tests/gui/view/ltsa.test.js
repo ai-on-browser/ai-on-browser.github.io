288ed53b242c3ee2eb7d86d663437c9ef062a913
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2022-12-25 20:30:36 +0900

    Add LTSA (#484)

---
+++ tests/gui/view/ltsa.test.js
@@ -0,0 +1,44 @@
+import puppeteer from 'puppeteer'
+
+import { getPage } from '../helper/browser'
+
+describe('dimensionality reduction', () => {
+	/** @type {puppeteer.Page} */
+	let page
+	beforeEach(async () => {
+		page = await getPage()
+	}, 10000)
+
+	afterEach(async () => {
+		await page?.close()
+	})
+
+	test('initialize', async () => {
+		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		taskSelectBox.select('DR')
+		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		modelSelectBox.select('ltsa')
+		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
+		const buttons = await methodMenu.waitForSelector('.buttons')
+
+		const k = await buttons.waitForSelector('input:nth-of-type(1)')
+		await expect((await k.getProperty('value')).jsonValue()).resolves.toBe('20')
+	}, 10000)
+
+	test('learn', async () => {
+		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		taskSelectBox.select('DR')
+		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		modelSelectBox.select('ltsa')
+		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
+		const buttons = await methodMenu.waitForSelector('.buttons')
+
+		const fitButton = await buttons.waitForSelector('input[value=Fit]')
+		await fitButton.evaluate(el => el.click())
+
+		const svg = await page.waitForSelector('#plot-area svg')
+		await svg.waitForSelector('.tile circle')
+		const circles = await svg.$$('.tile circle')
+		expect(circles).toHaveLength(300)
+	}, 60000)
+})
