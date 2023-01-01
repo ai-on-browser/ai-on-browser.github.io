9a73254ea3f1ddf99b1a5207f8b80141667942dc
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2023-01-01 20:48:10 +0900

    Fix LSA process if the argument is default (#506)
    * Fix LSA process if the argument is default
    
    * Return the input as is if the calculation result is the same as the input

--- js/view/lsa.js
+++ js/view/lsa.js
@@ -1,19 +1,16 @@
 import LSA from '../../lib/model/lsa.js'
+import Controller from '../controller.js'
 
-var dispLSA = function (elm, platform) {
-	elm.append('input')
-		.attr('type', 'button')
-		.attr('value', 'Fit')
-		.on('click', () => {
-			const dim = platform.dimension
-			const y = new LSA().predict(platform.trainInput, dim)
-			platform.trainResult = y
-		})
-}
-
-var lsa_init = function (platform) {
+export default function (platform) {
 	platform.setting.ml.usage = 'Click and add data point. Next, click "Fit" button.'
-	dispLSA(platform.setting.ml.configElement, platform)
+	platform.setting.ml.reference = {
+		title: 'Latent semantic analysis (Wikipedia)',
+		url: 'https://en.wikipedia.org/wiki/Latent_semantic_analysis',
+	}
+	const controller = new Controller(platform)
+	controller.input.button('Fit').on('click', () => {
+		const dim = platform.dimension
+		const y = new LSA().predict(platform.trainInput, dim)
+		platform.trainResult = y
+	})
 }
-
-export default lsa_init
