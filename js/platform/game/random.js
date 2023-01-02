44afd3f7841e6371fbbf5a465f2db48117b28d83
ishii-norimi <ishii-norimi@users.noreply.github.com>
2023-01-02 05:55:18 +0000

    deploy: 852faa3917f7f9f8e9a3bf3251f6f8079b4928fe

--- js/platform/game/random.js
+++ js/platform/game/random.js
@@ -1,1 +1,1 @@
-export default class RandomPlayer{constructor(){}set turn(t){this._turn=t}action(t,o){const s=t.choices(this._turn),e=s[Math.floor(Math.random()*s.length)];setTimeout((()=>{o(e)}),100)}close(){}}
+export default class RandomPlayer{constructor(){}set turn(t){this._turn=t}action(t){const o=t.choices(this._turn),e=o[Math.floor(Math.random()*o.length)];return new Promise((t=>setTimeout((()=>t(e)),100)))}close(){}}
