3fca738b8c52ae5c12542cc6b480d50999d71e85
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2023-02-05 14:27:53 +0900

    Add CLIQUE (#562)

---
+++ js/view/clique.js
@@ -0,0 +1,26 @@
+import CLIQUE from '../../lib/model/clique.js'
+import Controller from '../controller.js'
+
+export default function (platform) {
+	platform.setting.ml.usage = 'Click and add data point. Then, click "Fit" button.'
+	platform.setting.ml.reference = {
+		author: 'R. Agrawal, J. Gehrke, D. Gunopulos, P. Raghavan',
+		title: 'Automatic subspace clustering of high dimensional data for data mining applications',
+		year: 1998,
+	}
+	const controller = new Controller(platform)
+	const calc = () => {
+		const model = new CLIQUE(Array(platform.datas.dimension).fill(xi.value), threshold.value)
+		model.fit(platform.trainInput)
+		const pred = model.predict(platform.trainInput)
+		platform.trainResult = pred.map(v => v + 1)
+		const tilePred = model.predict(platform.testInput(4))
+		platform.testResult(tilePred.map(v => v < 0 ? -1 : v + 1))
+		clusters.value = model.size
+	}
+
+	const xi = controller.input.number({ label: 'step', min: 0, max: 100, step: 0.1, value: 0.2 })
+	const threshold = controller.input.number({ label: 'threshold', min: 0, max: 1, step: 0.01, value: 0.1 })
+	controller.input.button('Fit').on('click', calc)
+	const clusters = controller.text({ label: ' Clusters: ' })
+}
---
+++ tests/gui/view/clique.test.js
@@ -0,0 +1,46 @@
+import puppeteer from 'puppeteer'
+
+import { getPage } from '../helper/browser'
+
+describe('clustering', () => {
+	/** @type {puppeteer.Page} */
+	let page
+	beforeEach(async () => {
+		page = await getPage()
+	}, 10000)
+
+	afterEach(async () => {
+		await page?.close()
+	})
+
+	test('initialize', async () => {
+		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		taskSelectBox.select('CT')
+		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		modelSelectBox.select('clique')
+		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
+		const buttons = await methodMenu.waitForSelector('.buttons')
+
+		const step = await buttons.waitForSelector('input:nth-of-type(1)')
+		await expect((await step.getProperty('value')).jsonValue()).resolves.toBe('0.2')
+		const threshold = await buttons.waitForSelector('input:nth-of-type(2)')
+		await expect((await threshold.getProperty('value')).jsonValue()).resolves.toBe('0.1')
+	}, 10000)
+
+	test('learn', async () => {
+		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		taskSelectBox.select('CT')
+		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		modelSelectBox.select('clique')
+		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
+		const buttons = await methodMenu.waitForSelector('.buttons')
+
+		const clusters = await buttons.waitForSelector('span:last-child')
+		await expect(clusters.evaluate(el => el.textContent)).resolves.toBe('')
+
+		const fitButton = await buttons.waitForSelector('input[value=Fit]')
+		await fitButton.evaluate(el => el.click())
+
+		await expect(clusters.evaluate(el => el.textContent)).resolves.toMatch(/^[0-9]+$/)
+	}, 10000)
+})
