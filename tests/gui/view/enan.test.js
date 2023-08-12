8e0fcedcee2bb54f81de88339432263dffb4f9d3
ishii-norimi <mirasunimoni@yahoo.co.jp>
2023-08-12 21:30:05 +0900

    Add ENaN

---
+++ tests/gui/view/enan.test.js
@@ -0,0 +1,45 @@
+import { getPage } from '../helper/browser'
+
+describe('classification', () => {
+	/** @type {Awaited<ReturnType<getPage>>} */
+	let page
+	beforeEach(async () => {
+		page = await getPage()
+	}, 10000)
+
+	afterEach(async () => {
+		await page?.close()
+	})
+
+	test('initialize', async () => {
+		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		await taskSelectBox.selectOption('CF')
+		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		await modelSelectBox.selectOption('enan')
+		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
+		const buttons = await methodMenu.waitForSelector('.buttons')
+
+		const calc = await buttons.waitForSelector('input:nth-of-type(1)')
+		await expect((await calc.getProperty('value')).jsonValue()).resolves.toBe('Calculate')
+	}, 10000)
+
+	test('learn', async () => {
+		const dataSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(2) select')
+		await dataSelectBox.selectOption('uci')
+
+		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		await taskSelectBox.selectOption('CF')
+		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		await modelSelectBox.selectOption('enan')
+		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
+		const buttons = await methodMenu.waitForSelector('.buttons')
+
+		const methodFooter = await page.waitForSelector('#method_footer', { state: 'attached' })
+		await expect(methodFooter.evaluate(el => el.textContent)).resolves.toBe('')
+
+		const calculateButton = await buttons.waitForSelector('input[value=Calculate]')
+		await calculateButton.evaluate(el => el.click())
+
+		await expect(methodFooter.evaluate(el => el.textContent)).resolves.toMatch(/^Accuracy:[0-9.]+$/)
+	}, 10000)
+})
