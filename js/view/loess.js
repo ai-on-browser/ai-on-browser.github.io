3195069961d3fd3172795733b9c9abcafad670a9
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2023-11-08 22:39:29 +0900

    Add LOESS (#681)
    * Add LOESS
    
    * Add LOWESS test and bit improve
    
    * Fix class name

--- js/view/loess.js
+++ js/view/loess.js
@@ -1,11 +1,11 @@
-import LOWESS from '../../lib/model/lowess.js'
+import LOESS from '../../lib/model/loess.js'
 import Controller from '../controller.js'
 
 export default function (platform) {
 	platform.setting.ml.usage = 'Click and add data point. Next, click "Fit" button.'
 	const controller = new Controller(platform)
 	const fitModel = () => {
-		const model = new LOWESS()
+		const model = new LOESS()
 		model.fit(platform.trainInput, platform.trainOutput)
 		platform.testResult(model.predict(platform.testInput(10)))
 	}
