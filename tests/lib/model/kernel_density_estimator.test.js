235bd71b507f3f36b5c9dc8995f784089d2f72fb
ishii-norimi <mirasunimoni@yahoo.co.jp>
2023-11-18 19:11:42 +0900

    Move h parameter of KDE from fit method to constructor

--- tests/lib/model/kernel_density_estimator.test.js
+++ tests/lib/model/kernel_density_estimator.test.js
@@ -7,8 +7,27 @@ import KernelDensityEstimator from '../../../lib/model/kernel_density_estimator.
 import { correlation } from '../../../lib/evaluate/regression.js'
 
 describe('density estimation', () => {
+	test('default', () => {
+		const model = new KernelDensityEstimator()
+		const n = 500
+		const x = Matrix.concat(Matrix.randn(n, 2, 0, 0.1), Matrix.randn(n, 2, 5, 0.1)).toArray()
+
+		model.fit(x)
+		const y = model.predict(x)
+		expect(y).toHaveLength(x.length)
+
+		const p = []
+		for (let i = 0; i < x.length; i++) {
+			const p1 = Math.exp(-x[i].reduce((s, v) => s + v ** 2, 0) / (2 * 0.1)) / (2 * Math.PI * 0.1)
+			const p2 = Math.exp(-x[i].reduce((s, v) => s + (v - 5) ** 2, 0) / (2 * 0.1)) / (2 * Math.PI * 0.1)
+			p[i] = (p1 + p2) / 2
+		}
+		const corr = correlation(y, p)
+		expect(corr).toBeGreaterThan(0.9)
+	})
+
 	test.each([undefined, 'gaussian', 'triangular', 'epanechnikov', 'biweight', 'triweight'])('kernel %s', kernel => {
-		const model = new KernelDensityEstimator(kernel)
+		const model = new KernelDensityEstimator(0, kernel)
 		const n = 500
 		const x = Matrix.concat(Matrix.randn(n, 2, 0, 0.1), Matrix.randn(n, 2, 5, 0.1)).toArray()
 
@@ -27,7 +46,7 @@ describe('density estimation', () => {
 	})
 
 	test.each(['rectangular'])('kernel %s', kernel => {
-		const model = new KernelDensityEstimator(kernel)
+		const model = new KernelDensityEstimator(0, kernel)
 		const n = 500
 		const x = Matrix.concat(Matrix.randn(n, 2, 0, 0.1), Matrix.randn(n, 2, 5, 0.1)).toArray()
 
@@ -46,7 +65,7 @@ describe('density estimation', () => {
 	})
 
 	test('custom kernel', () => {
-		const model = new KernelDensityEstimator(v => 1 / (v + 1.0e-8))
+		const model = new KernelDensityEstimator(1.0, v => 1 / (v + 1.0e-8))
 		const n = 500
 		const x = Matrix.concat(Matrix.randn(n, 2, 0, 0.1), Matrix.randn(n, 2, 5, 0.1)).toArray()
 
