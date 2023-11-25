037eecb94c60f58770b04b6907f218fd0321c2d5
ishii-norimi <mirasunimoni@yahoo.co.jp>
2023-11-25 18:33:53 +0900

    Improve RNN layers

--- tests/lib/model/nns/layer/lstm.test.js
+++ tests/lib/model/nns/layer/lstm.test.js
@@ -8,9 +8,15 @@ import Tensor from '../../../../../lib/util/tensor.js'
 import LSTMLayer from '../../../../../lib/model/nns/layer/lstm.js'
 
 describe('layer', () => {
-	test('construct', () => {
-		const layer = new LSTMLayer({ size: 4 })
-		expect(layer).toBeDefined()
+	describe('construct', () => {
+		test('default', () => {
+			const layer = new LSTMLayer({ size: 4 })
+			expect(layer).toBeDefined()
+		})
+
+		test('invalid sequence', () => {
+			expect(() => new LSTMLayer({ size: 4, sequence_dim: 2 })).toThrow('Invalid sequence dimension')
+		})
 	})
 
 	describe('calc', () => {
@@ -36,6 +42,14 @@ describe('layer', () => {
 			const y = layer.calc(x)
 			expect(y.sizes).toEqual([10, 7, 4])
 		})
+
+		test('tensor sequence_dim: 0', () => {
+			const layer = new LSTMLayer({ size: 4, sequence_dim: 0 })
+
+			const x = Tensor.randn([7, 10, 5])
+			const y = layer.calc(x)
+			expect(y.sizes).toEqual([10, 4])
+		})
 	})
 
 	describe('grad', () => {
@@ -60,6 +74,17 @@ describe('layer', () => {
 			const bi = layer.grad(bo)
 			expect(bi.sizes).toEqual([10, 7, 5])
 		})
+
+		test('sequence_dim: 0', () => {
+			const layer = new LSTMLayer({ size: 4, sequence_dim: 0 })
+
+			const x = Tensor.randn([7, 10, 5])
+			layer.calc(x)
+
+			const bo = Matrix.ones(10, 4)
+			const bi = layer.grad(bo)
+			expect(bi.sizes).toEqual([7, 10, 5])
+		})
 	})
 
 	test('toObject', () => {
@@ -69,19 +94,6 @@ describe('layer', () => {
 		expect(obj.type).toBe('lstm')
 		expect(obj.return_sequences).toBeFalsy()
 		expect(obj.size).toBe(4)
-		for (let i = 0; i < 4; i++) {
-			expect(obj.b_z[0][i]).toBe(0)
-			expect(obj.b_in[0][i]).toBe(0)
-			expect(obj.b_for[0][i]).toBe(0)
-			expect(obj.b_out[0][i]).toBe(0)
-			expect(obj.r_z[i]).toHaveLength(4)
-			expect(obj.r_in[i]).toHaveLength(4)
-			expect(obj.r_for[i]).toHaveLength(4)
-			expect(obj.r_out[i]).toHaveLength(4)
-			expect(obj.p_in[0][i]).toBe(0)
-			expect(obj.p_for[0][i]).toBe(0)
-			expect(obj.p_out[0][i]).toBe(0)
-		}
 	})
 
 	test('fromObject', () => {
@@ -98,8 +110,67 @@ describe('nn', () => {
 		const x = Tensor.random([1, 7, 5], -0.1, 0.1)
 		const t = Matrix.random(1, 4, -0.8, 0.8)
 
+		for (let i = 0; i < 1000; i++) {
+			const loss = net.fit(x, t, 100, 0.01)
+			if (loss[0] < 1.0e-8) {
+				break
+			}
+		}
+
+		const y = net.calc(x)
+		for (let i = 0; i < t.cols; i++) {
+			expect(y.at(0, i)).toBeCloseTo(t.at(0, i))
+		}
+	})
+
+	test('string parameters', () => {
+		const net = NeuralNetwork.fromObject(
+			[
+				{ type: 'input', name: 'in' },
+				{ type: 'variable', value: Matrix.randn(5, 3), name: 'w_z' },
+				{ type: 'variable', value: Matrix.randn(5, 3), name: 'w_in' },
+				{ type: 'variable', value: Matrix.randn(5, 3), name: 'w_for' },
+				{ type: 'variable', value: Matrix.randn(5, 3), name: 'w_out' },
+				{ type: 'variable', value: Matrix.randn(3, 3), name: 'r_z' },
+				{ type: 'variable', value: Matrix.randn(3, 3), name: 'r_in' },
+				{ type: 'variable', value: Matrix.randn(3, 3), name: 'r_for' },
+				{ type: 'variable', value: Matrix.randn(3, 3), name: 'r_out' },
+				{ type: 'variable', value: Matrix.randn(1, 3), name: 'p_in' },
+				{ type: 'variable', value: Matrix.randn(1, 3), name: 'p_for' },
+				{ type: 'variable', value: Matrix.randn(1, 3), name: 'p_out' },
+				{ type: 'variable', value: Matrix.randn(1, 3), name: 'b_z' },
+				{ type: 'variable', value: Matrix.randn(1, 3), name: 'b_in' },
+				{ type: 'variable', value: Matrix.randn(1, 3), name: 'b_for' },
+				{ type: 'variable', value: Matrix.randn(1, 3), name: 'b_out' },
+				{
+					type: 'lstm',
+					size: 3,
+					w_z: 'w_z',
+					w_in: 'w_in',
+					w_for: 'w_for',
+					w_out: 'w_out',
+					r_z: 'r_z',
+					r_in: 'r_in',
+					r_for: 'r_for',
+					r_out: 'r_out',
+					p_in: 'p_in',
+					p_for: 'p_for',
+					p_out: 'p_out',
+					b_z: 'b_z',
+					b_in: 'b_in',
+					b_for: 'b_for',
+					b_out: 'b_out',
+					input: 'in',
+				},
+			],
+			'mse',
+			'adam'
+		)
+		const x = Tensor.random([1, 4, 5], -0.1, 0.1)
+		const t = Matrix.random(1, 3, -0.8, 0.8)
+
 		for (let i = 0; i < 100; i++) {
-			const loss = net.fit(x, t, 1000, 0.01)
+			const loss = net.fit(x, t, 100, 0.01)
 			if (loss[0] < 1.0e-8) {
 				break
 			}
--- tests/lib/model/nns/layer/rnn.test.js
+++ tests/lib/model/nns/layer/rnn.test.js
@@ -5,9 +5,15 @@ import Tensor from '../../../../../lib/util/tensor.js'
 import RNNLayer from '../../../../../lib/model/nns/layer/rnn.js'
 
 describe('layer', () => {
-	test('construct', () => {
-		const layer = new RNNLayer({ size: 4 })
-		expect(layer).toBeDefined()
+	describe('construct', () => {
+		test('default', () => {
+			const layer = new RNNLayer({ size: 4 })
+			expect(layer).toBeDefined()
+		})
+
+		test('invalid sequence', () => {
+			expect(() => new RNNLayer({ size: 4, sequence_dim: 2 })).toThrow('Invalid sequence dimension')
+		})
 	})
 
 	describe('calc', () => {
@@ -32,6 +38,14 @@ describe('layer', () => {
 			const x = Tensor.randn([10, 7, 5])
 			const y = layer.calc(x)
 			expect(y.sizes).toEqual([10, 7, 4])
+		})
+
+		test('tensor sequence_dim: 0', () => {
+			const layer = new RNNLayer({ size: 4, sequence_dim: 0 })
+
+			const x = Tensor.randn([7, 10, 5])
+			const y = layer.calc(x)
+			expect(y.sizes).toEqual([10, 4])
 		})
 
 		test('tensor no activation', () => {
@@ -74,6 +88,17 @@ describe('layer', () => {
 			expect(bi.sizes).toEqual([10, 7, 5])
 		})
 
+		test('sequence_dim: 0', () => {
+			const layer = new RNNLayer({ size: 4, sequence_dim: 0 })
+
+			const x = Tensor.randn([7, 10, 5])
+			layer.calc(x)
+
+			const bo = Matrix.ones(10, 4)
+			const bi = layer.grad(bo)
+			expect(bi.sizes).toEqual([7, 10, 5])
+		})
+
 		test('no activation', () => {
 			const layer = new RNNLayer({ size: 4, activation: null })
 
@@ -104,11 +129,6 @@ describe('layer', () => {
 		expect(obj.type).toBe('rnn')
 		expect(obj.return_sequences).toBeFalsy()
 		expect(obj.size).toBe(4)
-		for (let i = 0; i < 4; i++) {
-			expect(obj.b_xh[0][i]).toBe(0)
-			expect(obj.b_hh[0][i]).toBe(0)
-			expect(obj.w_hh[i]).toHaveLength(4)
-		}
 	})
 
 	test('fromObject', () => {
@@ -127,6 +147,35 @@ describe('nn', () => {
 
 		for (let i = 0; i < 100; i++) {
 			const loss = net.fit(x, t, 1000, 0.01)
+			if (loss[0] < 1.0e-8) {
+				break
+			}
+		}
+
+		const y = net.calc(x)
+		for (let i = 0; i < t.cols; i++) {
+			expect(y.at(0, i)).toBeCloseTo(t.at(0, i))
+		}
+	})
+
+	test('string parameters', () => {
+		const net = NeuralNetwork.fromObject(
+			[
+				{ type: 'input', name: 'in' },
+				{ type: 'variable', value: Matrix.randn(5, 3), name: 'w_x' },
+				{ type: 'variable', value: Matrix.randn(3, 3), name: 'w_h' },
+				{ type: 'variable', value: Matrix.randn(1, 3), name: 'b_x' },
+				{ type: 'variable', value: Matrix.randn(1, 3), name: 'b_h' },
+				{ type: 'rnn', size: 3, w_x: 'w_x', w_h: 'w_h', b_x: 'b_x', b_h: 'b_h', input: 'in' },
+			],
+			'mse',
+			'adam'
+		)
+		const x = Tensor.random([1, 4, 5], -0.1, 0.1)
+		const t = Matrix.random(1, 3, -0.8, 0.8)
+
+		for (let i = 0; i < 100; i++) {
+			const loss = net.fit(x, t, 100, 0.01)
 			if (loss[0] < 1.0e-8) {
 				break
 			}
