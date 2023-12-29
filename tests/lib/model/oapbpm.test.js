2692871e319338be5620a98aee09fd47dc362b1a
ishii-norimi <mirasunimoni@yahoo.co.jp>
2023-12-29 17:30:51 +0900

    Add OAP-BPM

---
+++ tests/lib/model/oapbpm.test.js
@@ -0,0 +1,24 @@
+import Matrix from '../../../lib/util/matrix.js'
+import OAPBPM from '../../../lib/model/oapbpm.js'
+
+import { rmse } from '../../../lib/evaluate/regression.js'
+
+describe('ordinal', () => {
+	test('fit', () => {
+		const model = new OAPBPM(10, 0.3)
+		const x = Matrix.concat(
+			Matrix.concat(Matrix.randn(50, 2, -5, 0.2), Matrix.randn(50, 2, 0, 0.2)),
+			Matrix.concat(Matrix.randn(50, 2, 5, 0.2), Matrix.randn(50, 2, 10, 0.2))
+		).toArray()
+		const t = []
+		for (let i = 0; i < x.length; i++) {
+			t[i] = Math.floor(i / 50)
+		}
+		for (let i = 0; i < 100; i++) {
+			model.fit(x, t)
+		}
+		const y = model.predict(x)
+		const err = rmse(y, t)
+		expect(err).toBeLessThan(0.5)
+	})
+})
