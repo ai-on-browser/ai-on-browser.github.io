9545c4ea985853794d6eeb97be7484bc0a1f85f2
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2024-01-12 21:46:11 +0900

    Fix RBP (#751)
    * Fix RBP
    
    * Fix test

--- tests/lib/model/rbp.test.js
+++ tests/lib/model/rbp.test.js
@@ -1,3 +1,6 @@
+import { jest } from '@jest/globals'
+jest.retryTimes(3)
+
 import Matrix from '../../../lib/util/matrix.js'
 import RBP from '../../../lib/model/rbp.js'
 
@@ -16,4 +19,20 @@ test('fit', () => {
 	const y = model.predict(x)
 	const acc = accuracy(y, t)
 	expect(acc).toBeGreaterThan(0.95)
+})
+
+test('remove sv', () => {
+	const model = new RBP(10)
+	const x = Matrix.concat(Matrix.randn(50, 2, 0, 0.2), Matrix.randn(50, 2, 5, 0.2)).toArray()
+	x[50] = [0.1, 0.1]
+	const t = []
+	for (let i = 0; i < x.length; i++) {
+		t[i] = Math.floor(i / 50) * 2 - 1
+	}
+	for (let i = 0; i < 10; i++) {
+		model.fit(x, t)
+	}
+	const y = model.predict(x)
+	const acc = accuracy(y, t)
+	expect(acc).toBeGreaterThan(0.8)
 })
