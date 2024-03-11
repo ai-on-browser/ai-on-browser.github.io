40ed827b4dde04d107e22a4a0caa37167e6dc388
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2024-03-11 23:17:41 +0900

    Fix train output data of passing_bablok (#807)

--- js/view/passing_bablok.js
+++ js/view/passing_bablok.js
@@ -1,26 +1,20 @@
 import PassingBablok from '../../lib/model/passing_bablok.js'
-
-var dispPB = function (elm, platform) {
-	const fitModel = cb => {
-		const dim = platform.datas.dimension
-		const model = new PassingBablok()
-		model.fit(
-			platform.trainInput.map(v => v[0]),
-			platform.trainInput.map(v => v[0])
-		)
-		platform.testResult(model.predict(platform.testInput(1).map(v => v[0])))
-	}
-
-	elm.append('input')
-		.attr('type', 'button')
-		.attr('value', 'Fit')
-		.on('click', () => fitModel())
-}
+import Controller from '../controller.js'
 
 export default function (platform) {
 	platform.setting.ml.usage = 'Click and add data point. Next, click "Fit" button.'
 	platform.setting.ml.require = {
 		dimension: 1,
 	}
-	dispPB(platform.setting.ml.configElement, platform)
+	const controller = new Controller(platform)
+	const fitModel = () => {
+		const model = new PassingBablok()
+		model.fit(
+			platform.trainInput.map(v => v[0]),
+			platform.trainOutput.map(v => v[0])
+		)
+		platform.testResult(model.predict(platform.testInput(1).map(v => v[0])))
+	}
+
+	controller.input.button('Fit').on('click', fitModel)
 }
