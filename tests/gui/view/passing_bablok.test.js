f2ad596fac746cd50f0fe2ca8d2750c3dc6482ee
ishii-norimi <mirasunimoni@yahoo.co.jp>
2024-03-11 23:00:18 +0900

    Fix train output data of passing_bablok

---
+++ tests/gui/view/passing_bablok.test.js
@@ -0,0 +1,41 @@
+import { getPage } from '../helper/browser'
+
+describe('regression', () => {
+	/** @type {Awaited<ReturnType<getPage>>} */
+	let page
+	beforeEach(async () => {
+		page = await getPage()
+	})
+
+	afterEach(async () => {
+		await page?.close()
+	})
+
+	// eslint-disable-next-line jest/expect-expect
+	test('initialize', async () => {
+		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		await taskSelectBox.selectOption('RG')
+		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		await modelSelectBox.selectOption('passing_bablok')
+		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
+		const buttons = await methodMenu.waitForSelector('.buttons')
+
+		await buttons.waitForSelector('input[value=Fit]')
+	})
+
+	test('learn', async () => {
+		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		await taskSelectBox.selectOption('RG')
+		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		await modelSelectBox.selectOption('passing_bablok')
+		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
+		const buttons = await methodMenu.waitForSelector('.buttons')
+
+		const fitButton = await buttons.waitForSelector('input[value=Fit]')
+		await fitButton.evaluate(el => el.click())
+
+		const svg = await page.waitForSelector('#plot-area svg')
+		await svg.waitForSelector('.tile-render image')
+		expect((await svg.$$('.tile-render image')).length).toBeGreaterThan(0)
+	})
+})
