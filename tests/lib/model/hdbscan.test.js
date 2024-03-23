286a77d5901794668b3be7350034f5b5bdb47237
ishii-norimi <mirasunimoni@yahoo.co.jp>
2024-03-23 12:11:00 +0900

    Fix test

--- tests/lib/model/hdbscan.test.js
+++ tests/lib/model/hdbscan.test.js
@@ -28,13 +28,7 @@ describe('clustering', () => {
 		expect(ri).toBeGreaterThan(0.7)
 	})
 
-	test.each([
-		undefined,
-		'euclid',
-		'manhattan',
-		'chebyshev',
-		(a, b) => a.reduce((s, v, i) => s + Math.exp((v - b[i]) ** 2) - 1, 0),
-	])('%s', metric => {
+	test.each([undefined, 'euclid', 'manhattan', 'chebyshev'])('%s', metric => {
 		const model = new HDBSCAN(undefined, 2, metric)
 		const n = 50
 		const x = Matrix.concat(
@@ -46,6 +40,27 @@ describe('clustering', () => {
 		const y = model.predict(x)
 		expect(y).toHaveLength(x.length)
 		expect(model.size).toBeGreaterThanOrEqual(3)
+
+		const t = []
+		for (let i = 0; i < x.length; i++) {
+			t[i] = Math.floor(i / n)
+		}
+		const ri = randIndex(y, t)
+		expect(ri).toBeGreaterThan(0.9)
+	})
+
+	test.each([(a, b) => a.reduce((s, v, i) => s + Math.exp((v - b[i]) ** 2) - 1, 0)])('%s', metric => {
+		const model = new HDBSCAN(undefined, 2, metric)
+		const n = 50
+		const x = Matrix.concat(
+			Matrix.concat(Matrix.randn(n, 2, 0, 0.2), Matrix.randn(n, 2, 5, 0.2)),
+			Matrix.randn(n, 2, [0, 5], 0.2)
+		).toArray()
+		expect(model.size).toBe(0)
+
+		const y = model.predict(x)
+		expect(y).toHaveLength(x.length)
+		expect(model.size).toBeGreaterThanOrEqual(2)
 
 		const t = []
 		for (let i = 0; i < x.length; i++) {
