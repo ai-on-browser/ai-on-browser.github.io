c1f8db5b76feb6fe4a3185706e68ea30dbcbb514
ishii-norimi <mirasunimoni@yahoo.co.jp>
2024-07-13 21:15:20 +0900

    Add DiSH

---
+++ js/view/dish.js
@@ -0,0 +1,24 @@
+import DiSH from '../../lib/model/dish.js'
+import Controller from '../controller.js'
+
+export default function (platform) {
+	platform.setting.ml.usage = 'Click and add data point. Then, click "Fit" button.'
+	platform.setting.ml.reference = {
+		author: 'E. Achtert, C. Bohm, H. P. Kriegel, P. Kroger, I. Muller-Gorman, A. Zimek',
+		title: 'Detection and Visualization of Subspace Cluster Hierarchies',
+		year: 2007,
+	}
+	const controller = new Controller(platform)
+
+	const fitModel = () => {
+		let model = new DiSH(mu.value, e.value)
+
+		const pred = model.predict(platform.trainInput).map(v => v + 1)
+		platform.trainResult = pred
+	}
+
+	const mu = controller.input.number({ label: ' mu ', min: 1, max: 1000, value: 20 })
+	const e = controller.input.number({ label: ' e ', min: 0, max: 100, step: 0.1, value: 0.1 })
+
+	controller.input.button('Fit').on('click', fitModel)
+}
