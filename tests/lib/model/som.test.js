28398a3921e23111f706e1e703be5a30a32f6fa7
ishii-norimi <mirasunimoni@yahoo.co.jp>
2025-05-03 20:14:30 +0900

    improve some test

--- tests/lib/model/som.test.js
+++ tests/lib/model/som.test.js
@@ -7,26 +7,68 @@ import SOM from '../../../lib/model/som.js'
 import { randIndex } from '../../../lib/evaluate/clustering.js'
 import { coRankingMatrix } from '../../../lib/evaluate/dimensionality_reduction.js'
 
-test('clustering', () => {
-	const model = new SOM(2, 1, 3)
-	const n = 50
-	const x = Matrix.concat(
-		Matrix.concat(Matrix.randn(n, 2, 0, 0.1), Matrix.randn(n, 2, 5, 0.1)),
-		Matrix.randn(n, 2, [0, 5], 0.1)
-	).toArray()
+describe('clustering', () => {
+	test('default', () => {
+		const model = new SOM(2, 1)
+		const n = 50
+		const x = Matrix.concat(
+			Matrix.concat(Matrix.randn(n, 2, 0, 0.1), Matrix.randn(n, 2, 5, 0.1)),
+			Matrix.randn(n, 2, [0, 5], 0.1)
+		).toArray()
 
-	for (let i = 0; i < 100; i++) {
-		model.fit(x)
-	}
-	const y = model.predict(x)
-	expect(y).toHaveLength(x.length)
+		for (let i = 0; i < 100; i++) {
+			model.fit(x)
+		}
+		const y = model.predict(x)
+		expect(y).toHaveLength(x.length)
 
-	const t = []
-	for (let i = 0; i < x.length; i++) {
-		t[i] = Math.floor(i / n)
-	}
-	const ri = randIndex(y, t)
-	expect(ri).toBeGreaterThan(0.9)
+		expect([...new Set(y)].length).toBeLessThanOrEqual(20)
+	})
+
+	test('init PCA', () => {
+		const model = new SOM(2, 1, 3)
+		const n = 50
+		const x = Matrix.concat(
+			Matrix.concat(Matrix.randn(n, 2, 0, 0.1), Matrix.randn(n, 2, 5, 0.1)),
+			Matrix.randn(n, 2, [0, 5], 0.1)
+		).toArray()
+
+		for (let i = 0; i < 100; i++) {
+			model.fit(x)
+		}
+		const y = model.predict(x)
+		expect(y).toHaveLength(x.length)
+
+		const t = []
+		for (let i = 0; i < x.length; i++) {
+			t[i] = Math.floor(i / n)
+		}
+		const ri = randIndex(y, t)
+		expect(ri).toBeGreaterThan(0.9)
+	})
+
+	test('init random', () => {
+		const model = new SOM(2, 1, 3)
+		model._init_method = 'random'
+		const n = 50
+		const x = Matrix.concat(
+			Matrix.concat(Matrix.randn(n, 2, 0, 0.1), Matrix.randn(n, 2, 5, 0.1)),
+			Matrix.randn(n, 2, [0, 5], 0.1)
+		).toArray()
+
+		for (let i = 0; i < 100; i++) {
+			model.fit(x)
+		}
+		const y = model.predict(x)
+		expect(y).toHaveLength(x.length)
+
+		const t = []
+		for (let i = 0; i < x.length; i++) {
+			t[i] = Math.floor(i / n)
+		}
+		const ri = randIndex(y, t)
+		expect(ri).toBeGreaterThan(0.9)
+	})
 })
 
 test('dimension reduction', () => {
