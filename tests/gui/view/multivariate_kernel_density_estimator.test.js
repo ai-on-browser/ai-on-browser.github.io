b0650331af4faafb24e90e7cafd7bda178b9502e
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2025-10-26 19:54:21 +0900

    Add multivariate kernel density estimator (#1013)

---
+++ tests/gui/view/multivariate_kernel_density_estimator.test.js
@@ -0,0 +1,37 @@
+import { getPage } from '../helper/browser'
+
+describe('density estimation', () => {
+	/** @type {Awaited<ReturnType<getPage>>} */
+	let page
+	beforeEach(async () => {
+		page = await getPage()
+		const taskSelectBox = page.locator('#ml_selector dl:first-child dd:nth-child(5) select')
+		await taskSelectBox.selectOption('DE')
+		const modelSelectBox = page.locator('#ml_selector .model_selection #mlDisp')
+		await modelSelectBox.selectOption('multivariate_kernel_density_estimator')
+	})
+
+	afterEach(async () => {
+		await page?.close()
+	})
+
+	test('initialize', async () => {
+		const methodMenu = page.locator('#ml_selector #method_menu')
+		const buttons = methodMenu.locator('.buttons')
+
+		const method = buttons.locator('select:nth-of-type(1)')
+		await expect(method.inputValue()).resolves.toBe('silverman')
+	})
+
+	test('learn', async () => {
+		const methodMenu = page.locator('#ml_selector #method_menu')
+		const buttons = methodMenu.locator('.buttons')
+
+		const fitButton = buttons.locator('input[value=Fit]')
+		await fitButton.dispatchEvent('click')
+
+		const svg = page.locator('#plot-area svg')
+		const img = svg.locator('.tile-render image')
+		await expect(img.count()).resolves.toBeGreaterThan(0)
+	})
+})
---
+++ tests/lib/model/multivariate_kernel_density_estimator.test.js
@@ -0,0 +1,32 @@
+import { jest } from '@jest/globals'
+jest.retryTimes(3)
+
+import Matrix from '../../../lib/util/matrix.js'
+import MultivariateKernelDensityEstimator from '../../../lib/model/multivariate_kernel_density_estimator.js'
+
+import { correlation } from '../../../lib/evaluate/regression.js'
+
+describe('density estimation', () => {
+	test.each([undefined, 'silverman', 'scott'])('%p', method => {
+		const model = new MultivariateKernelDensityEstimator(method)
+		const sgm = Matrix.fromArray([
+			[0.1, 0],
+			[0, 0.5],
+		])
+		const x = Matrix.randn(500, 2, 0, sgm.toArray()).toArray()
+
+		model.fit(x)
+		const y = model.predict(x)
+		expect(y).toHaveLength(x.length)
+
+		const p = []
+		for (let i = 0; i < x.length; i++) {
+			const pi =
+				Math.exp(-x[i].reduce((s, v, d) => s + v ** 2 / sgm.at(d, d), 0) / 2) /
+				Math.sqrt((2 * Math.PI) ** 2 * sgm.det())
+			p[i] = pi / 2
+		}
+		const corr = correlation(y, p)
+		expect(corr).toBeGreaterThan(0.9)
+	})
+})
