8c4912a715aa9a6e984658a19db3292ef9a30d19
ishii-norimi <mirasunimoni@yahoo.co.jp>
2025-11-30 17:34:19 +0900

    Fix w type 1 calculation of lagrange model

--- tests/lib/model/lagrange.test.js
+++ tests/lib/model/lagrange.test.js
@@ -23,3 +23,25 @@ test.each([undefined, '', 'weighted', 'newton'])('interpolation %s', { retry: 3 
 	const err = rmse(y0, x0.map(Math.sin))
 	expect(err).toBeLessThan(0.1)
 })
+
+test('interpolation w type 1', () => {
+	const model = new LagrangeInterpolation()
+	model._w_type = 1
+	const x = Matrix.random(20, 1, -2, 2).value
+	const t = []
+	for (let i = 0; i < x.length; i++) {
+		t[i] = Math.sin(x[i])
+	}
+	model.fit(x, t)
+
+	const y = model.predict(x)
+	expect(y).toHaveLength(x.length)
+	for (let i = 0; i < y.length; i++) {
+		expect(y[i]).toBeCloseTo(t[i])
+	}
+
+	const x0 = Matrix.random(100, 1, -2, 2).value
+	const y0 = model.predict(x0)
+	const err = rmse(y0, x0.map(Math.sin))
+	expect(err).toBeLessThan(0.1)
+})
