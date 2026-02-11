34ed135efba206358e64fba99b5b3169436e50d0
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2026-02-11 19:47:20 +0900

    Show centroids of BMS (#1055)

--- js/view/bms.js
+++ js/view/bms.js
@@ -7,6 +7,16 @@ export default function (platform) {
 
 	let model = null
 
+	const pred = () => {
+		const pred = model.predict()
+		platform.trainResult = pred.map(v => v + 1)
+		clusters.value = model.size
+		platform.centroids(
+			model._c,
+			model._c.map((_, i) => i + 1)
+		)
+	}
+
 	const h = controller.input.number({ label: 'h', min: 0, max: 10, step: 0.01, value: 0.05 })
 	const threshold = controller.input.number({ label: 'threshold', min: 0, max: 10, step: 0.01, value: 0.01 })
 	controller
@@ -14,18 +24,14 @@ export default function (platform) {
 		.init(() => {
 			model = new BlurringMeanShift(h.value, threshold.value)
 			model.init(platform.trainInput)
-			const pred = model.predict()
-			platform.trainResult = pred.map(v => v + 1)
-			clusters.value = model.size
+			pred()
 		})
 		.step(() => {
 			if (model === null) {
 				return
 			}
 			model.fit()
-			const pred = model.predict()
-			platform.trainResult = pred.map(v => v + 1)
-			clusters.value = model.size
+			pred()
 		})
 	const clusters = controller.text({ label: ' clusters ', value: 0 })
 }
