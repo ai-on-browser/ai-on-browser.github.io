a2fe163bf1d5eb56e0c26dcefdf846b9949cceda
ishii-norimi <mirasunimoni@yahoo.co.jp>
2026-03-01 15:55:59 +0900

    Fix and add some tests

--- tests/gui/view/agglomerative.test.js
+++ tests/gui/view/agglomerative.test.js
@@ -5,9 +5,9 @@ describe('clustering', () => {
 	let page
 	beforeEach(async () => {
 		page = await getPage()
-		const taskSelectBox = await page.waitForSelector('#ml_selector dl:first-child dd:nth-child(5) select')
+		const taskSelectBox = page.locator('#ml_selector dl:first-child dd:nth-child(5) select')
 		await taskSelectBox.selectOption('CT')
-		const modelSelectBox = await page.waitForSelector('#ml_selector .model_selection #mlDisp')
+		const modelSelectBox = page.locator('#ml_selector .model_selection #mlDisp')
 		await modelSelectBox.selectOption('agglomerative')
 	})
 
@@ -16,30 +16,30 @@ describe('clustering', () => {
 	})
 
 	test('initialize', async () => {
-		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
-		const buttons = await methodMenu.waitForSelector('.buttons')
+		const methodMenu = page.locator('#ml_selector #method_menu')
+		const buttons = methodMenu.locator('.buttons')
 
-		const method = await buttons.waitForSelector('select:nth-of-type(1)')
-		await expect((await method.getProperty('value')).jsonValue()).resolves.toBe('Complete Linkage')
-		const metrix = await buttons.waitForSelector('select:nth-of-type(2)')
-		await expect((await metrix.getProperty('value')).jsonValue()).resolves.toBe('euclid')
-		const clusters = await buttons.waitForSelector('input:nth-of-type(2)')
-		await expect(clusters.getAttribute('value')).resolves.toBe('1')
+		const method = buttons.locator('select:nth-of-type(1)')
+		await expect(method.inputValue()).resolves.toBe('Complete Linkage')
+		const metrix = buttons.locator('select:nth-of-type(2)')
+		await expect(metrix.inputValue()).resolves.toBe('euclid')
+		const clusters = buttons.locator('input:nth-of-type(2)')
+		await expect(clusters.inputValue()).resolves.toBe('1')
 	})
 
 	test('learn', async () => {
-		const methodMenu = await page.waitForSelector('#ml_selector #method_menu')
-		const buttons = await methodMenu.waitForSelector('.buttons')
+		const methodMenu = page.locator('#ml_selector #method_menu')
+		const buttons = methodMenu.locator('.buttons')
 
-		const initButton = await buttons.waitForSelector('input[value=Initialize]')
-		await initButton.evaluate(el => el.click())
+		const initButton = buttons.locator('input[value=Initialize]')
+		await initButton.dispatchEvent('click')
 
-		const clusters = await buttons.waitForSelector('input:nth-of-type(2)')
-		await expect((await clusters.getProperty('value')).jsonValue()).resolves.toBe('10')
+		const clusters = buttons.locator('input:nth-of-type(2)')
+		await expect(clusters.inputValue()).resolves.toBe('10')
 		await expect(clusters.getAttribute('min')).resolves.toBe('1')
 		await expect(clusters.getAttribute('max')).resolves.toBe('300')
-		const crange = await buttons.waitForSelector('input:nth-of-type(3)')
-		await expect((await clusters.getProperty('value')).jsonValue()).resolves.toBe('10')
+		const crange = buttons.locator('input:nth-of-type(3)')
+		await expect(crange.inputValue()).resolves.toBe('10')
 		await expect(crange.getAttribute('min')).resolves.toBe('1')
 		await expect(crange.getAttribute('max')).resolves.toBe('300')
 	})
