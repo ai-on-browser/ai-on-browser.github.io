884622128f5ce36a0d3567873dcca6a90223410f
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2026-03-07 22:28:54 +0900

    Add Phillips k-Means algorithm (#1083)
    * Add Phillips k-Means algorithm
    
    * Refactor prediction handling in Phillips k-Means algorithm to use model's predict method without input parameters

---
+++ js/view/phillips_kmeans.js
@@ -0,0 +1,37 @@
+import PhillipsKMeans from '../../lib/model/phillips_kmeans.js'
+import Controller from '../controller.js'
+
+export default function (platform) {
+	platform.setting.ml.usage = 'Click and add data point. Then, click "Fit" button.'
+	platform.setting.ml.reference = {
+		author: 'S. J. Phillips',
+		title: 'Acceleration of K-Means and Related Clustering Algorithms',
+		year: 2002,
+	}
+	const controller = new Controller(platform)
+	let model = null
+
+	const fitModel = () => {
+		if (!model) {
+			model = new PhillipsKMeans(k.value)
+			model.init(platform.trainInput)
+		}
+		model.fit()
+		const pred = model.predict()
+		platform.trainResult = pred.map(v => v + 1)
+		platform.centroids(
+			model.centroids,
+			model.centroids.map((_, i) => i + 1),
+			{ line: true }
+		)
+	}
+
+	const k = controller.input.number({ label: ' k ', min: 1, max: 1000, value: 3 })
+	controller
+		.stepLoopButtons()
+		.init(() => {
+			model = null
+		})
+		.step(fitModel)
+		.epoch()
+}
---
+++ tests/lib/model/phillips_kmeans.test.js
@@ -0,0 +1,28 @@
+import { randIndex } from '../../../lib/evaluate/clustering.js'
+import PhillipsKMeans from '../../../lib/model/phillips_kmeans.js'
+import Matrix from '../../../lib/util/matrix.js'
+
+describe('clustering', { retry: 3 }, () => {
+	test('default', () => {
+		const model = new PhillipsKMeans(20)
+		const n = 20
+		const x = Array.from({ length: 20 }, (_, i) => i * 2)
+			.reduce((mat, m) => Matrix.concat(mat, Matrix.randn(n, 2, m, 0.1)), new Matrix(0, 2))
+			.toArray()
+
+		model.init(x)
+		expect(model.centroids).toHaveLength(20)
+		for (let i = 0; i < 10; i++) {
+			model.fit()
+		}
+		const y = model.predict(x)
+		expect(y).toHaveLength(x.length)
+
+		const t = []
+		for (let i = 0; i < x.length; i++) {
+			t[i] = Math.floor(i / n)
+		}
+		const ri = randIndex(y, t)
+		expect(ri).toBeGreaterThan(0.9)
+	})
+})
