400c0c34722616bb7bab61677c421d519fcc9536
ishii-norimi <ishii-norimi@users.noreply.github.com>
2026-03-22 04:02:02 +0000

    deploy: 7e3ce7e316a736cfcada372dacc84481c14cc82b

--- js/view/lasso.js
+++ js/view/lasso.js
@@ -1,4 +1,4 @@
-var m=Object.defineProperty;var a=(t,e)=>m(t,"name",{value:e,configurable:!0});import k from"../../lib/model/lasso.js";import b from"../../lib/util/matrix.js";import h from"../controller.js";export default function g(t){t.setting.ml.usage='Click and add data point. Next, click "Initialize". Finally, click "Fit" button repeatedly.',t.setting.ml.reference={title:"Lasso (Wikipedia)",url:"https://en.wikipedia.org/wiki/Lasso_(statistics)"},t.setting.ml.detail=`
+var $=Object.defineProperty;var a=(t,i)=>$(t,"name",{value:i,configurable:!0});import k from"../../lib/model/lasso.js";import m from"../controller.js";export default function b(t){t.setting.ml.usage='Click and add data point. Next, click "Initialize". Finally, click "Fit" button repeatedly.',t.setting.ml.reference={title:"Lasso (Wikipedia)",url:"https://en.wikipedia.org/wiki/Lasso_(statistics)"},t.setting.ml.detail=`
 The model form is
 $$
 f(X) = X W + \\epsilon
@@ -9,4 +9,4 @@ $$
 L(W) = \\| X W - y \\|^2 + \\lambda \\| W \\|_1
 $$
 where $ y $ is the observed value corresponding to $ X $.
-`;const e=new h(t);let i=null;const c=t.task,l=a(()=>{if(c==="FS"){i.fit(t.trainInput,t.trainOutput);const s=i.importance().map((n,o)=>[n,o]);s.sort((n,o)=>o[0]-n[0]);const u=t.dimension,p=s.map(n=>n[1]).slice(0,u),$=b.fromArray(t.trainInput);t.trainResult=$.col(p).toArray()}else{i.fit(t.trainInput,t.trainOutput);const s=i.predict(t.testInput(4));t.testResult(s)}},"fitModel"),r=e.select(["CD","ISTA","LARS"]),d=e.select({label:"lambda = ",values:[0,1e-4,.001,.01,.1,1,10,100]});e.stepLoopButtons().init(()=>{i=new k(+d.value,r.value),t.init()}).step(l).epoch()}a(g,"default");
+`;const i=new m(t);let n=null;const c=t.task,l=a(()=>{if(c==="FS"){n.fit(t.trainInput,t.trainOutput);const o=n.importance().map((e,s)=>[e,s]);o.sort((e,s)=>s[0]-e[0]);const r=t.dimension,p=o.map(e=>e[1]).slice(0,r);t.trainResult=t.trainInput.map(e=>p.map(s=>e[s]))}else{n.fit(t.trainInput,t.trainOutput);const o=n.predict(t.testInput(4));t.testResult(o)}},"fitModel"),d=i.select(["CD","ISTA","LARS"]),u=i.select({label:"lambda = ",values:[0,1e-4,.001,.01,.1,1,10,100]});i.stepLoopButtons().init(()=>{n=new k(+u.value,d.value),t.init()}).step(l).epoch()}a(b,"default");
