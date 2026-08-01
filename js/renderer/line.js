80637001db83df314a90615b05fe9f7aac4ef5bf
ishii-norimi <mirasunimoni@yahoo.co.jp>
2026-08-01 14:46:37 +0900

    Fix scale calculation for rendering grids

--- js/renderer/line.js
+++ js/renderer/line.js
@@ -375,8 +375,11 @@ export default class LineRenderer extends BaseRenderer {
 
 	_getScales(min, max) {
 		const diff = max - min
-		if (diff === 0) {
+		if (!Number.isFinite(diff)) {
 			return []
+		}
+		if (diff === 0) {
+			return [min]
 		}
 		let s = Math.floor(Math.log10(diff))
 		let step = 10 ** s
