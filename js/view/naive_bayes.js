673fd5d4065517d7489b07654f19612fea391adf
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2026-08-25 21:56:28 +0900

    Add multinomial naive bayes (#1188)

--- js/view/naive_bayes.js
+++ js/view/naive_bayes.js
@@ -1,4 +1,5 @@
 import NaiveBayes from '../../lib/model/naive_bayes.js'
+import Matrix from '../../lib/util/matrix.js'
 import Controller from '../controller.js'
 
 export default function (platform) {
@@ -8,21 +9,38 @@ export default function (platform) {
 		url: 'https://en.wikipedia.org/wiki/Naive_Bayes_classifier',
 	}
 	const controller = new Controller(platform)
-	const model = new NaiveBayes()
 
 	const calcBayes = () => {
+		const model = new NaiveBayes({ name: dist.value, a: a.value })
+		let tx = platform.trainInput
+		let px = platform.testInput(3)
+		if (dist.value === 'multinomial') {
+			const d = discrete.value
+			const x = Matrix.fromArray(tx)
+			const max = x.max()
+			const min = x.min()
+			tx = tx.map(r => r.map(v => Math.floor(((v - min) / (max - min)) * d)))
+			px = px.map(r => r.map(v => Math.floor(((v - min) / (max - min)) * d)))
+		}
 		model.fit(
-			platform.trainInput,
+			tx,
 			platform.trainOutput.map(v => v[0])
 		)
 		if (platform.task === 'DE') {
-			const pred = model.probability(platform.testInput(3)).map(p => p.reduce((s, v) => s + v, 0))
+			const pred = model.probability(px).map(p => p.reduce((s, v) => s + v, 0))
 			platform.testResult(pred)
 		} else {
-			platform.testResult(model.predict(platform.testInput(3)))
+			platform.testResult(model.predict(px))
 		}
 	}
 
-	controller.select({ label: 'Distribution ', values: ['gaussian'] }).on('change', calcBayes)
+	const dist = controller.select({ label: 'Distribution ', values: ['gaussian', 'multinomial'] }).on('change', () => {
+		multinomialConf.element.style.display = dist.value === 'multinomial' ? 'inline' : 'none'
+		calcBayes()
+	})
+	const multinomialConf = controller.span()
+	multinomialConf.element.style.display = 'none'
+	const discrete = multinomialConf.input.number({ label: 'discrete', max: 100, min: 2, value: 10 })
+	const a = multinomialConf.input.number({ label: 'alpha', max: 10, min: 0, step: 0.1, value: 1 })
 	controller.input.button('Calculate').on('click', calcBayes)
 }
