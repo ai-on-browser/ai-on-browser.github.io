5654cb9fc499a23e47773e1a55bd8916475bca11
Ishii Norimi <47292902+ishii-norimi@users.noreply.github.com>
2026-09-25 14:04:18 +0900

    Remove Matrix class dependency, fix ISODATA and LGB (#1218)

--- tests/lib/model/split_merge.test.js
+++ tests/lib/model/split_merge.test.js
@@ -1,24 +1,50 @@
 import { randIndex } from '../../../lib/evaluate/clustering.js'
 import SplitAndMerge from '../../../lib/model/split_merge.js'
-import Matrix from '../../../lib/util/matrix.js'
 
-test.each([undefined, 'variance', 'uniformity'])('predict %s', { timeout: 10000 }, method => {
-	const model = new SplitAndMerge(method)
-	const n = 100
-	const x = Matrix.zeros(n, n).toArray()
-	const t = []
-	for (let i = 0, p = 0; i < n; i++) {
-		for (let j = 0; j < n; j++, p++) {
-			if ((i - n / 2) ** 2 + (j - n / 2) ** 2 < (n / 4) ** 2) {
-				x[i][j] = 255
-				t[p] = 1
-			} else {
-				t[p] = 0
+describe.each([undefined, 'variance', 'uniformity'])('predict %s', { timeout: 10000 }, method => {
+	test('2d', () => {
+		const model = new SplitAndMerge(method)
+		const n = 100
+		const x = []
+		const t = []
+		for (let i = 0, p = 0; i < n; i++) {
+			x[i] = []
+			for (let j = 0; j < n; j++, p++) {
+				if ((i - n / 2) ** 2 + (j - n / 2) ** 2 < (n / 4) ** 2) {
+					x[i][j] = 255
+					t[p] = 1
+				} else {
+					x[i][j] = 0
+					t[p] = 0
+				}
 			}
 		}
-	}
 
-	const y = model.predict(x)
-	const ri = randIndex(y.flat(), t)
-	expect(ri).toBeGreaterThan(0.9)
+		const y = model.predict(x)
+		const ri = randIndex(y.flat(), t)
+		expect(ri).toBeGreaterThan(0.9)
+	})
+
+	test('3d', () => {
+		const model = new SplitAndMerge(method)
+		const n = 50
+		const x = []
+		const t = []
+		for (let i = 0, p = 0; i < n; i++) {
+			x[i] = []
+			for (let j = 0; j < n; j++, p++) {
+				if ((i - n / 2) ** 2 + (j - n / 2) ** 2 < (n / 4) ** 2) {
+					x[i][j] = [255, 255, 0]
+					t[p] = 1
+				} else {
+					x[i][j] = [0, 0, 0]
+					t[p] = 0
+				}
+			}
+		}
+
+		const y = model.predict(x)
+		const ri = randIndex(y.flat(), t)
+		expect(ri).toBeGreaterThan(0.9)
+	})
 })
